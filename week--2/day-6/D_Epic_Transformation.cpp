d4eea2f3bce7084e19d6efb272565825fa567641
Shuchi28 <seven77bd@gmail.com>
2024-11-08 23:44:01 +0600

    Add files via upload
    weel-2 day-6

---
+++ week--2/day-6/D_Epic_Transformation.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        map<int, int> mp;
+        for (int i=0; i<n; i++)
+        {
+            int x;
+            cin >> x;
+            mp[x]++;
+        }
+
+        priority_queue<int> pq;
+        for (auto a : mp)
+        {
+            pq.push(a.second);
+        }
+        
+        while (!pq.empty())
+        {
+            if (pq.size()<2)
+            {
+                break;
+            }
+            int x, y;
+            x = pq.top();
+            pq.pop();
+            y = pq.top();
+            pq.pop();
+            x--;
+            y--;
+            if (x>0)
+            {
+                pq.push(x);
+            }
+            if (y>0)
+            {
+                pq.push(y);
+            }
+            
+        }
+        int ans = 0;
+        while (!pq.empty())
+        {
+            ans += pq.top();
+            pq.pop();
+        }
+        
+        cout << ans <<'\n';
+    }
+    
+
+    return 0;
+}

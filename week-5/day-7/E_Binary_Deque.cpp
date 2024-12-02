044253c8a988ac34db6ce556beecb7588e133cab
Shuchi28 <seven77bd@gmail.com>
2024-12-02 18:46:07 +0600

    Update E_Binary_Deque.cpp

--- week-5/day-7/E_Binary_Deque.cpp
+++ week-5/day-7/E_Binary_Deque.cpp
@@ -13,6 +13,40 @@ int main()
         int n, s;
         cin >> n >> s;
         
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+        }
+        
+        int l=0, r=0, k=0, ans=INT_MIN, a=0;
+        while (r<n)
+        {
+            k += arr[r];
+            a += arr[r];
+            if(k<=s)
+            {
+                ans = max(ans, r-l+1);
+            }
+            else{
+                while (k>s && l<=r)
+                {
+                    k -= arr[l];
+                    l++;
+                }
+                if(k<=s)
+                {
+                    ans = max(ans, r-l+1);
+                }
+            }
+            r++;
+        }
+
+        if(a<s)
+            cout << -1 << '\n';
+        else
+            cout << n-ans << '\n';
+        
     }
     
 

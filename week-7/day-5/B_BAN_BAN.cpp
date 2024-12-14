f88495f3d564fe96e433356dbd15dffa34223ef7
Shuchi28 <seven77bd@gmail.com>
2024-12-14 15:09:14 +0600

    Update B_BAN_BAN.cpp

--- week-7/day-5/B_BAN_BAN.cpp
+++ week-7/day-5/B_BAN_BAN.cpp
@@ -3,7 +3,25 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
 
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int firstB=1, lastA=(3*n)-1;
+        cout << (n+1)/2 <<'\n';
+        while (lastA>firstB)
+        {
+            cout <<firstB <<" " <<lastA <<'\n';
+            firstB +=3;
+            lastA -=3;
+        }
+        
+    }
+    
 
     return 0;
 }

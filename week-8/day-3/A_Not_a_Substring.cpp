37af08ea7c3d6d43ca329a4a8883a45e7f25e0fe
Shuchi28 <seven77bd@gmail.com>
2024-12-20 04:18:00 +0600

    Update A_Not_a_Substring.cpp

--- week-8/day-3/A_Not_a_Substring.cpp
+++ week-8/day-3/A_Not_a_Substring.cpp
@@ -1,8 +1,68 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string ss;
+        cin >> ss;
+
+        bool flag = true;
+        int n = ss.length();
+        for (int i=0; i<n-1; i++)
+        {
+            if (ss[i]==ss[i+1])
+            {
+                flag = false;
+                break;
+            }
+        }
+
+        if (!flag)
+        {
+            cout << "YES" <<endl;
+            for (int i=0; i<n; i++)
+            {
+                cout << "()";
+            }
+            cout <<endl;
+            continue;
+        }
+        if (n==2)
+        {
+            if (ss == "()")
+            {
+                cout << "NO" <<endl;
+                continue;
+            }
+             
+            cout << "YES" <<endl;
+            cout << "(())" <<endl;
+            continue;
+        }
+
+        cout << "YES" <<endl;
+            
+        for (int i=0; i<n; i++)
+        {
+            cout << "(";
+        }
+        for (int i=0; i<n; i++)
+        {
+            cout << ")";
+        }
+        cout <<endl;
+        
+    }
 
 
     return 0;

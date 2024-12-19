e75ab73faf13f0456d5f6d5edd74239185ab4a4c
Shuchi28 <seven77bd@gmail.com>
2024-12-19 11:28:44 +0600

    Update B_Battle_for_Survive.cpp

--- week-8/day-3/B_Battle_for_Survive.cpp
+++ week-8/day-3/B_Battle_for_Survive.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int main()
 {
-
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        long long sum=0, slast, last;
+        for (int i=0; i<n-2; i++)
+        {
+            int x;
+            cin >> x;
+            sum += x;
+        }
+        cin >> slast;
+        slast -= sum;
+        cin >> last;
+        last -= slast;
+        
+        cout << last <<'\n';
+    }
+    
 
     return 0;
 }

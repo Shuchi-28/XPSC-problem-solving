b3e62ad12b28b3574bbe8bdb8f8a0ed947227096
Shuchi28 <seven77bd@gmail.com>
2024-12-20 23:41:03 +0600

    Update OR_Permutation.cpp

--- week-8/day-5/OR_Permutation.cpp
+++ week-8/day-5/OR_Permutation.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 int main()
 {
+   int t;
+   cin >> t;
+   while (t--)
+   {
+        int n;
+        cin >> n;
 
-
+        for (int i=n; i>=1; i--)
+        {
+            cout << i <<" ";
+        }
+        cout << '\n';
+        
+   }
     return 0;
 }

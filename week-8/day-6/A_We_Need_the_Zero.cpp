272824cd8f9af89a07dd0c6dbfe9c7a87967d7ec
Shuchi28 <seven77bd@gmail.com>
2024-12-23 01:04:53 +0600

    Update A_We_Need_the_Zero.cpp

--- week-8/day-6/A_We_Need_the_Zero.cpp
+++ week-8/day-6/A_We_Need_the_Zero.cpp
@@ -3,7 +3,34 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, XOR=0;
+        cin >> n;
 
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+            XOR ^= arr[i];
+        }
+        if (n%2 == 0)
+        {
+            if (XOR == 0)
+            {
+                cout << 0 <<'\n';
+            }
+            else{
+                cout << -1 <<'\n';
+            }
+        }
+        else{
+            cout << XOR <<'\n';
+        }
+    }
+    
 
     return 0;
 }

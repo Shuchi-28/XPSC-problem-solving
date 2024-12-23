4fc3398404da53219d333d533cc4f88a9267a356
Shuchi28 <seven77bd@gmail.com>
2024-12-23 21:55:23 +0600

    Update A_XORwice.cpp

--- week-8/day-6/A_XORwice.cpp
+++ week-8/day-6/A_XORwice.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main()
 {
-
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << (a^b) <<'\n';
+    }
+    
 
     return 0;
 }

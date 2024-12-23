f960fea4b5c0d65cf855e2619beb7138eee37d47
Shuchi28 <seven77bd@gmail.com>
2024-12-24 01:14:18 +0600

    Update Dull_Operation.cpp

--- week-8/day-6/Dull_Operation.cpp
+++ week-8/day-6/Dull_Operation.cpp
@@ -3,7 +3,17 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
 
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        cout << n << " " << n-1 <<'\n';
+    }
+    
 
     return 0;
 }

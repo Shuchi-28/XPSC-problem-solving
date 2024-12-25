a3a12a83f3f2f7a14937a8969069f97ba04f3660
Shuchi28 <seven77bd@gmail.com>
2024-12-25 16:17:34 +0600

    Update A_Phone_Desktop.cpp

--- week-9/day-1/A_Phone_Desktop.cpp
+++ week-9/day-1/A_Phone_Desktop.cpp
@@ -3,7 +3,40 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y, ans=0;
+        cin >> x >> y;
+
+        while (x>0 || y>0)
+        {
+            if (y>=2)
+            {
+                y -= 2;
+                x -= 7;
+                ans++;
+            }
+            else if (y==1)
+            {
+                y -= 1;
+                x -= 11;
+                ans++;
+            }
+            else{
+                x -= 15;
+                ans++;
+            }
+        }
+        
+        cout << ans <<'\n';
+        
+    }
+    
 
     return 0;
 }

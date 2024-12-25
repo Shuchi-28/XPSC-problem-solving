51fc0b0fc521646cb3046ef440aba311d423e192
Shuchi28 <seven77bd@gmail.com>
2024-12-25 12:51:51 +0600

    Update A_Bazoka_and_Mocha_s_Array.cpp

--- week-9/day-1/A_Bazoka_and_Mocha_s_Array.cpp
+++ week-9/day-1/A_Bazoka_and_Mocha_s_Array.cpp
@@ -3,7 +3,40 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
 
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for (int i=0; i<n; i++)
+        {
+            cin >> a[i];
+        }
+        
+        int flag=0;
+        for (int i=0; i<n; i++)
+        {
+            if (is_sorted(a.begin(),a.end()))
+            {
+                flag = 1;
+            }
+            rotate(a.begin(),a.begin()+1, a.end());
+        }
+
+        if (flag==0)
+        {
+            cout << "No" <<'\n';
+        }
+        else{
+            cout << "Yes\n";
+        }
+        
+    }
+    
 
     return 0;
 }

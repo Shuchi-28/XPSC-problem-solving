a3c3f47b16a2c94dca132340700a2f0961daea0d
Shuchi28 <seven77bd@gmail.com>
2024-12-25 17:27:11 +0600

    Update A_Little_Nikita.cpp

--- week-9/day-2/A_Little_Nikita.cpp
+++ week-9/day-2/A_Little_Nikita.cpp
@@ -3,7 +3,33 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        if (n<m)
+        {
+            cout << "No" <<'\n';
+        }
+        else{
+            if ((m-n)%2 == 0)
+            {
+                cout << "Yes\n";
+            }
+            else{
+                cout << "No\n";
+            }
+            
+        }
+        
+    }
 
     return 0;
 }

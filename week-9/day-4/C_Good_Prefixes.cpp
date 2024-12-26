a8f91baeb4ec3b50dfc1fabdd39a48be4bdfb9d2
Shuchi28 <seven77bd@gmail.com>
2024-12-27 01:26:54 +0600

    Update C_Good_Prefixes.cpp

--- week-9/day-4/C_Good_Prefixes.cpp
+++ week-9/day-4/C_Good_Prefixes.cpp
@@ -3,7 +3,38 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, cnt=0;
+        cin >> n;
+        vector<long long> v(n);
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
+        int mx = INT_MIN;
+        long long sum = 0;
+
+        for (int i=0; i<n; i++)
+        {
+            sum += v[i];
+            if (v[i]>mx)
+                mx = v[i];
+            if (2*mx == sum)
+            {
+                cnt++;
+            }
+        }
+        
+        cout << cnt <<'\n';
+
+    }
+    
 
     return 0;
 }

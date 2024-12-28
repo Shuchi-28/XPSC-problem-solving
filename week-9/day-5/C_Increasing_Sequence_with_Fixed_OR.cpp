7cb1480d6b5105e34645414da2ffe7e08003df21
Shuchi28 <seven77bd@gmail.com>
2024-12-28 22:58:26 +0600

    Update C_Increasing_Sequence_with_Fixed_OR.cpp

--- week-9/day-5/C_Increasing_Sequence_with_Fixed_OR.cpp
+++ week-9/day-5/C_Increasing_Sequence_with_Fixed_OR.cpp
@@ -3,7 +3,38 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        deque<long long> ans;
+        for (int i=0; i<=__lg(n); i++)
+        {
+            if ((n >> i) & 1)
+            {
+                long long value = n-(1LL << i);
+                if (value > 0)
+                {
+                    ans.push_front(value);
+                }
+                
+            }  
+        }
+        
+        ans.push_back(n);
+        cout << ans.size() <<'\n';
+        for (auto val : ans)
+        {
+            cout << val << " ";
+        }
+        cout <<"\n";
+    }
+    
 
     return 0;
 }

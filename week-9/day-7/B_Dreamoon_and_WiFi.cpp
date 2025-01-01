a7ff5c2a2cf32d247c0375e8796303cbde3a2eb0
Shuchi28 <seven77bd@gmail.com>
2025-01-01 23:22:38 +0600

    Update B_Dreamoon_and_WiFi.cpp

--- week-9/day-7/B_Dreamoon_and_WiFi.cpp
+++ week-9/day-7/B_Dreamoon_and_WiFi.cpp
@@ -3,7 +3,41 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    int ps1=0, ps2=0, uns2=0;
+    for (int i=0; i<s1.size(); i++)
+    {
+        if (s1[i]=='+')
+        {
+            ps1++;
+        }
+        if (s2[i]=='+')
+        {
+            ps2++;
+        }
+        if (s2[i]=='?')
+        {
+            uns2++;
+        }
+        
+    }
+    
+    int cnt = 0;
+    for (int i=0; i<(1LL<<uns2); i++)
+    {
+        if (__builtin_popcount(i)+ps2 == ps1)
+        {
+            cnt++;
+        }
+        
+    }
+    
+    cout << fixed<<setprecision(12) << (double)cnt/(1LL<<uns2) <<'\n';
 
     return 0;
 }

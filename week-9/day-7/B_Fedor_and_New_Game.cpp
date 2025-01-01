c6f6d419de1029da9f1b5dcac85325e2f7dad6d8
Shuchi28 <seven77bd@gmail.com>
2025-01-01 14:21:59 +0600

    Update B_Fedor_and_New_Game.cpp

--- week-9/day-7/B_Fedor_and_New_Game.cpp
+++ week-9/day-7/B_Fedor_and_New_Game.cpp
@@ -1,8 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+int arr[10000];
 
 int main()
 {
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    for (int i=0; i<=m; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    int ans = 0;
+    for (int i=0; i<m; i++)
+    {
+        int sum = 0;
+        for (int j=0; j<n; j++)
+        {
+            sum += (arr[m]>>j&1) != (arr[i]>>j&1);
+        }
+        ans += sum <= k;
+    }
+
+    cout << ans <<'\n';
 
 
     return 0;

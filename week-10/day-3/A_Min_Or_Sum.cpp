4116c0e1c0bc30902e6bcd89963d5ab6bb9fd0dc
Shuchi28 <seven77bd@gmail.com>
2025-01-02 22:21:00 +0600

    Update A_Min_Or_Sum.cpp

--- week-10/day-3/A_Min_Or_Sum.cpp
+++ week-10/day-3/A_Min_Or_Sum.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
 
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+        }
+        
+        int x = arr[0] | arr[1];
+        for (int i = 2; i<n; i++)
+        {
+            x = x | arr[i];
+        }
+        
+        cout << x <<'\n';
+
+    }
+    
 
     return 0;
 }

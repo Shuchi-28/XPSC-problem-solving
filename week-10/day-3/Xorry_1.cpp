2ebc92cd9d5a54534547e635b53b4e15054b9134
Shuchi28 <seven77bd@gmail.com>
2025-01-02 23:04:35 +0600

    Update Xorry_1.cpp

--- week-10/day-3/Xorry_1.cpp
+++ week-10/day-3/Xorry_1.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
 
+    while (t--)
+    {
+        int x;
+        cin >> x;
 
+        int a = __lg(x);
+        a = (1LL << a);
+
+        cout <<  (x-a) << " " << a <<'\n';
+
+    }
+    
     return 0;
 }

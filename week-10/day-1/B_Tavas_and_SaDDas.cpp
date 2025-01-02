9c5c4b7b1f0a9d525cb8d992374f9c98c43ca47a
Shuchi28 <seven77bd@gmail.com>
2025-01-02 11:27:46 +0600

    Update B_Tavas_and_SaDDas.cpp

--- week-10/day-1/B_Tavas_and_SaDDas.cpp
+++ week-10/day-1/B_Tavas_and_SaDDas.cpp
@@ -3,7 +3,28 @@ using namespace std;
 
 int main()
 {
+    string s;
+    cin >> s;
 
+    int n = s.size();
+    long long x=0, ans = 0;
+
+    for (int i=0; i<n; i++)
+    {
+        x += (1LL << i);
+    }
+    
+
+    for (int i=0; i<n; i++)
+    {
+        if (s[i]=='7')
+        {
+            ans += (1LL << (n-i-1));
+        }
+        
+    }
+
+    cout << x+ans; 
 
     return 0;
 }

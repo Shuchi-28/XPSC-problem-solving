2e8b436aed4512f898aa83ca85736dbd5a359fc0
Shuchi28 <seven77bd@gmail.com>
2025-01-03 16:58:04 +0600

    Update Xorry_2.cpp

--- week-10/day-3/Xorry_2.cpp
+++ week-10/day-3/Xorry_2.cpp
@@ -3,7 +3,36 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
 
+    while (t--)
+    {
+        long long int x;
+        cin >> x;
+
+        long long int a = __lg(x);
+        int cnt = 0;
+        bool flag = false;
+        for (int i=a-1; i>=0; i--)
+        {
+            if (x&(1<<i))
+            {
+                flag = true;
+            }
+            if (flag && !(x&(1<<i)))
+            {
+                cnt++;
+            }
+            
+        }
+        
+        cout << (1LL << cnt) <<'\n';
+
+    }
+    
     return 0;
 }

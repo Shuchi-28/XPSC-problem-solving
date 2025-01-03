e563032862520c38819ea9980e96bed10b8e8f52
Shuchi28 <seven77bd@gmail.com>
2025-01-03 20:58:13 +0600

    Update B_Rock_and_Lever.cpp

--- week-10/day-4/B_Rock_and_Lever.cpp
+++ week-10/day-4/B_Rock_and_Lever.cpp
@@ -3,7 +3,36 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+
+        map<long long int, long long int> mp;
+        for (int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+            int x = __lg(arr[i]) + 1;
+            mp[x]++;
+        }
+        
+        long long int ans = 0;
+        for(auto a: mp)
+        {
+            long long int s= a.second;
+            ans += s*(s-1)/2;
+        }
+
+        cout << ans <<'\n';
+
+    }
+    
 
     return 0;
 }

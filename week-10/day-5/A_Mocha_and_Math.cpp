b12b13a937d66a3c4ad3f73e3e340d46203f6dfb
Shuchi28 <seven77bd@gmail.com>
2025-01-05 01:12:32 +0600

    Update A_Mocha_and_Math.cpp

--- week-10/day-5/A_Mocha_and_Math.cpp
+++ week-10/day-5/A_Mocha_and_Math.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int ans;
+        cin >> ans;
+        for (int i=1; i<n; i++)
+        {
+            int x;
+            cin >> x;
+            ans &= x;
+        }
+
+        cout << ans <<'\n';
+        
+    }
+    
 
     return 0;
 }

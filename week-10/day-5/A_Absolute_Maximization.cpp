dea5c6e0c092f30f1afe0841a1f439dc112ba589
Shuchi28 <seven77bd@gmail.com>
2025-01-05 20:49:10 +0600

    Update A_Absolute_Maximization.cpp

--- week-10/day-5/A_Absolute_Maximization.cpp
+++ week-10/day-5/A_Absolute_Maximization.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        
+        int a, o;
+        cin >> arr[0];
+        a = arr[0], o = arr[0];
+        for (int i=1; i<n; i++)
+        {
+            cin >> arr[i];
+            a &= arr[i];
+            o |= arr[i];
+        }
+        
+        cout << o-a <<'\n';
+    }
+    
 
     return 0;
 }

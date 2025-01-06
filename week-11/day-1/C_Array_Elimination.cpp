bd3e44fcd52d426f1e60ccceae5c668fa440c5f5
Shuchi28 <seven77bd@gmail.com>
2025-01-06 23:34:49 +0600

    Update C_Array_Elimination.cpp

--- week-11/day-1/C_Array_Elimination.cpp
+++ week-11/day-1/C_Array_Elimination.cpp
@@ -10,7 +10,48 @@ int main()
     cin >> t;
     while (t--)
     {
-        
+        int n;
+        cin >> n;
+        int arr[n], bit[30];
+
+        for (int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+        }
+        for (int i=0; i<30; i++)
+        {
+            bit[i] = 0;
+        }
+
+        for (int i=0; i<30; i++)
+        {
+            long long num = (1LL << i);
+            for (int j=0; j<n; j++)
+            {
+                if ((arr[j]&num) != 0)
+                {
+                    bit[i]++;
+                }
+            }
+        }
+        for (int k=1; k<=n; k++)
+        {
+            bool flag=0;
+            for (int i=0; i<30; i++)
+            {
+                if ((bit[i]%k) != 0)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                cout << k <<" ";
+            }
+            
+        }
+        cout <<'\n';
         
     }
     

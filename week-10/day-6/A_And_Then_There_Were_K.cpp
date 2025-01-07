98a97473c3ca7a6a5774b3ae1762069471b1ef12
Shuchi28 <seven77bd@gmail.com>
2025-01-08 00:54:04 +0600

    Update A_And_Then_There_Were_K.cpp

--- week-10/day-6/A_And_Then_There_Were_K.cpp
+++ week-10/day-6/A_And_Then_There_Were_K.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int x = __lg(n);
+        cout << (1LL<<x)-1 <<'\n';
+    }
+    
 
     return 0;
 }

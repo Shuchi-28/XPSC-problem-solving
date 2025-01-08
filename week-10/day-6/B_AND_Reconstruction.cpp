d79eb4f2244453abcaa311ded86a9393c1d8e931
Shuchi28 <seven77bd@gmail.com>
2025-01-08 20:58:35 +0600

    Update B_AND_Reconstruction.cpp

--- week-10/day-6/B_AND_Reconstruction.cpp
+++ week-10/day-6/B_AND_Reconstruction.cpp
@@ -3,7 +3,48 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, flag=0;
+        cin >> n;
+
+        vector<long long> b(n-1), a(n);
+        for (int i=0; i<n-1; i++)
+        {
+            cin >> b[i];
+        }
+        a[0] = b[0];
+        a[n-1] = b[n-2];
+        for (int i=1; i<n-1; i++)
+        {
+            a[i] = b[i]|b[i-1];
+        }
+        for (int i=0; i<n-1; i++)
+        {
+            if (b[i] != (a[i]&a[i+1]))
+            {
+                flag = 1;
+            }
+            
+        }
+        if (flag == 1)
+        {
+            cout << -1;
+        }
+        else{
+            for(int i=0; i<n; i++)
+            {
+                cout << a[i] << " ";
+            }
+        }
+        cout <<'\n';
+    }
+    
 
     return 0;
 }

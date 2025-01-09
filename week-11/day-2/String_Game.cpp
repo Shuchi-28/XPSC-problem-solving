a95f304fc6f64f4d7e64cca200b820b41f8d8a1d
Shuchi28 <seven77bd@gmail.com>
2025-01-10 00:30:12 +0600

    Update String_Game.cpp

--- week-11/day-2/String_Game.cpp
+++ week-11/day-2/String_Game.cpp
@@ -3,6 +3,32 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        int one = count(s.begin(),s.end(),'1');
+        int zero = count(s.begin(),s.end(),'0');
+
+        int cnt = min(one, zero);
+        if (cnt%2 == 0)
+        {
+            cout << "Ramos" <<'\n';
+        }
+        else{
+            cout << "Zlatan" <<'\n';
+        }
+
+    }
 
 
     return 0;

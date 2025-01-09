4ef07594ff16f44fe4355896659a378809bf4aaf
Shuchi28 <seven77bd@gmail.com>
2025-01-09 08:28:10 +0600

    Update B_Binary_Colouring.cpp

--- week-10/day-6/B_Binary_Colouring.cpp
+++ week-10/day-6/B_Binary_Colouring.cpp
@@ -3,6 +3,53 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+        
+        vector<int> v(31,0);
+        for (int i=0; i<30; i++)
+        {
+            if(1LL & (x>>i))
+            {
+                if (v[i])
+                {
+                    v[i+1] = 1;
+                    v[i] = 0;
+                }
+                else if (i>0)
+                {
+                    if (v[i-1]==1)
+                    {
+                        v[i+1] = 1;
+                        v[i-1] = -1;
+                    }
+                    else{
+                        v[i] = 1;
+                    }
+                }
+                else if (i == 0)
+                {
+                    v[i] = 1;
+                }
+                
+            }
+            
+        }
+        cout << 31 <<'\n';
+        for (int a: v)
+        {
+            cout << a <<" ";
+        }
+        cout <<'\n';
+
+    }
 
 
     return 0;

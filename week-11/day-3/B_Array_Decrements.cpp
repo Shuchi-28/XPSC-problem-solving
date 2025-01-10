faa5778c1abc5646b12efd1ad393312f1634a321
Shuchi28 <seven77bd@gmail.com>
2025-01-10 19:35:43 +0600

    Update B_Array_Decrements.cpp

--- week-11/day-3/B_Array_Decrements.cpp
+++ week-11/day-3/B_Array_Decrements.cpp
@@ -10,6 +10,45 @@ int main()
     cin >> t;
     while (t--)
     {
+        int n, flag=0, zeros=0, diff=INT_MIN;
+        cin >> n;
+        int a[n], b[n];
+        for (int i=0; i<n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int i=0; i<n; i++)
+        {
+            cin >> b[i];
+            if(b[i]==0)
+                zeros++;
+        }
+
+        for (int i=0; i<n; i++)
+        {
+            if (a[i]<b[i])
+            {
+                flag = 1;
+                break;
+            }
+            diff = max(a[i]-b[i], diff);
+        }
+        for (int i=0; i<n; i++)
+        {
+            if ((a[i]-diff) != b[i] && (b[i]!=0))
+            {
+                flag = 1;
+                break;
+            }
+        }
+        
+        if (flag==0)
+        {
+            cout << "YES\n";
+        }
+        else{
+            cout << "NO" <<'\n';
+        }
         
         
     }

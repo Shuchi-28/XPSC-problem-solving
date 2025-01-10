1eea860786c882df83c3843093d3ce73e9d912e0
Shuchi28 <seven77bd@gmail.com>
2025-01-10 19:37:36 +0600

    Update C_Grandma_Capa_Knits_a_Scarf.cpp

--- week-11/day-3/C_Grandma_Capa_Knits_a_Scarf.cpp
+++ week-11/day-3/C_Grandma_Capa_Knits_a_Scarf.cpp
@@ -10,7 +10,56 @@ int main()
     cin >> t;
     while (t--)
     {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        vector<int> cnt(26, INT_MAX);
+        set<char> st(s.begin(), s.end());
         
+        for (char x: st)
+        {
+            int i=0, j=n-1, c=0;
+            while (i<j)
+            {
+                if (s[i]==s[j])
+                {
+                    i++;
+                    j--;
+                }
+                else
+                {
+                    if (s[i]==x)
+                    {
+                        i++;
+                        c++;
+                    }
+                    else if (s[j]==x)
+                    {
+                        j--;
+                        c++;
+                    }
+                    else{
+                        c = -1;
+                        break;
+                    }
+                }
+                
+            }
+            if(c!=-1)
+                cnt[x-'a'] = c;
+        }
+        
+        sort(cnt.begin(), cnt.end());
+        if (cnt[0]==INT_MAX)
+        {
+            cout << -1 <<'\n';
+        }
+        else{
+            cout << cnt[0] <<'\n';
+        }
         
     }
     

42b25d3b924aef027a6cafe0383f420e2395a023
Shuchi28 <seven77bd@gmail.com>
2025-01-12 03:12:20 +0600

    Update B_3_palindrome.cpp

--- week-11/day-4/B_3_palindrome.cpp
+++ week-11/day-4/B_3_palindrome.cpp
@@ -6,7 +6,29 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
+    int n,f=0;
+    cin >> n;
+    string s = "";
 
+    for (int i=0; i<n/2; i++)
+    {
+        if(i%2==0)
+            s += "aa";
+        else
+            s += "bb";
+    }
+    
+    if (n%2==1 && ((n/2)%2)==0)
+    {
+        s += "a";
+    }
+    else if (n%2==1 && ((n/2)%2)==1)
+    {
+        s += "b";
+    }
+    
+    cout << s <<'\n';    
+    
+    
     return 0;
 }

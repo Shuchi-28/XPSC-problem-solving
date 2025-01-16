7b6191e4c05940c94c43768cccfbd0f1cfeac107
Shuchi28 <seven77bd@gmail.com>
2025-01-16 09:11:42 +0600

    Update B_Swap_and_Delete.cpp

--- week-11/day-7/B_Swap_and_Delete.cpp
+++ week-11/day-7/B_Swap_and_Delete.cpp
@@ -3,7 +3,46 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+
+        int n = s.length();
+        int zero = count(s.begin(), s.end(), '0');
+        int one = count(s.begin(), s.end(), '1');
+
+        int i;
+        for (i=0; i<n; i++)
+        {
+            if (s[i]=='0')
+            {
+                if (one>0)
+                    one--;
+                else{
+                    break;
+                }
+            }
+            else if (s[i]=='1')
+            {
+                if (zero>0)
+                    zero--;
+                else{
+                    break;
+                }
+            }
+            
+        }
+
+        cout << n-i <<'\n';
+        
+    }   
+    
 
     return 0;
 }

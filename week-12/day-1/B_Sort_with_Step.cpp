841da6809740e69b04a8834800a694672c7da11e
Shuchi28 <seven77bd@gmail.com>
2025-01-17 01:57:29 +0600

    Update B_Sort_with_Step.cpp

--- week-12/day-1/B_Sort_with_Step.cpp
+++ week-12/day-1/B_Sort_with_Step.cpp
@@ -3,7 +3,39 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k, cnt=0;
+        cin >> n >> k;
 
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+            if (abs(arr[i]-i-1)%k != 0)
+            {
+                cnt++;
+            }
+        }
+        
+        if (cnt==0)
+        {
+            cout << "0\n";
+        }
+        else if (cnt==2)
+        {
+            cout << "1\n";
+        }
+        else{
+            cout << "-1\n";
+        }
+
+    }
+    
     return 0;
 }

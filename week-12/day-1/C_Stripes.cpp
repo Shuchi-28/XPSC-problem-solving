53acfed91ff1d7619b2964577d39173f15b3a376
Shuchi28 <seven77bd@gmail.com>
2025-01-17 02:21:28 +0600

    Update C_Stripes.cpp

--- week-12/day-1/C_Stripes.cpp
+++ week-12/day-1/C_Stripes.cpp
@@ -3,7 +3,37 @@ using namespace std;
 
 int main()
 {
-    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int flag=0, cntR=0;
+        for (int i=0; i<8; i++)
+        {
+            for (int j=0; j<8; j++)
+            {
+                char f;
+                cin >> f;
+                if(f=='R')
+                    cntR++;
+            }
+            if(cntR==8)
+                flag = 1;
+            else
+                cntR = 0;
+        }
+        
+        if(flag)
+            cout << "R" <<endl;
+        else
+            cout << "B" <<endl;
+        
+    }
+
 
     return 0;
 }

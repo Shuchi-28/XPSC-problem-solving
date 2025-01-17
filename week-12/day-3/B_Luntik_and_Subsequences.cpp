4ed81ef4c6e7e219dc88c5ac6d66f762e274c7dc
Shuchi28 <seven77bd@gmail.com>
2025-01-17 12:58:59 +0600

    Update B_Luntik_and_Subsequences.cpp

--- week-12/day-3/B_Luntik_and_Subsequences.cpp
+++ week-12/day-3/B_Luntik_and_Subsequences.cpp
@@ -11,7 +11,26 @@ int main()
 
     while (t--)
     {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
         
+        long long one = count(v.begin(), v.end(), 1);
+        long long zero = count(v.begin(), v.end(), 0);
+
+        if (one == 0)
+        {
+            cout << "0\n";
+        }
+        else{
+            cout << one*(1LL<<zero) <<'\n';
+        }
+
         
     }
 

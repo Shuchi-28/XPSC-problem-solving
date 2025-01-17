fc7af68957bcf3f63eb3c26371b62fc6f04503ba
Shuchi28 <seven77bd@gmail.com>
2025-01-17 12:00:46 +0600

    Update B_Make_It_Increasing.cpp

--- week-12/day-2/B_Make_It_Increasing.cpp
+++ week-12/day-2/B_Make_It_Increasing.cpp
@@ -3,7 +3,47 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, ans=0;
+        cin >> n;
+
+        int v[n];
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
+        
+        int flag=0;
+        for (int i=n-2; i>=0; i--)
+        {
+            while (v[i]>=v[i+1] && v[i]>0)
+            {
+                v[i]/=2;
+                ans++;
+            }
+            if (v[i]>=v[i+1])
+            {
+                flag = 1;
+                break;
+            }
+                
+        }   
+        if (flag==1)
+        {
+            cout << "-1\n";
+        }
+        else{
+            cout << ans <<'\n';
+        }
+       
+    }
+    
 
     return 0;
 }

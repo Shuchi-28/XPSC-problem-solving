447cac815be4e5653c77863102fa08d43b911234
Shuchi28 <seven77bd@gmail.com>
2025-01-21 03:05:28 +0600

    Update G_Fall_Down.cpp

--- week-12/day-5/G_Fall_Down.cpp
+++ week-12/day-5/G_Fall_Down.cpp
@@ -11,8 +11,58 @@ int main()
 
     while (t--)
     {
-        
+        int n, m;
+        cin >> n >> m;
+        vector<vector<char>> arr(n,vector<char>(m));
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<m; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
 
+        vector<vector<char>> ans(n,vector<char>(m,'.'));
+
+        for (int j=0; j<m; j++)
+        {
+            int sum = 0;
+            for (int i=0; i<n; i++)
+            {
+                if (arr[i][j]=='*')
+                {
+                    sum++;
+                }
+                else if (arr[i][j]=='o')
+                {
+                    ans[i][j] = 'o';
+                    for (int k=1; k<=sum; k++)
+                    {
+                        ans[i-k][j] = '*';
+                    }
+                    sum = 0;
+                }
+                
+            }
+            if (sum)
+            {
+                for (int k=1; k<=sum; k++)
+                {
+                    ans[n-k][j] = '*';
+                }
+            }
+            
+        }
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<m; j++)
+            {
+                cout << ans[i][j];
+            }
+            cout <<'\n';
+        }
+
+        cout <<'\n';
     }
     
 

c5d5eecdca6a29afbd38e225d33128575f953be6
Shuchi28 <seven77bd@gmail.com>
2025-01-21 02:08:10 +0600

    Update E_2_Letter_Strings.cpp

--- week-12/day-4/E_2_Letter_Strings.cpp
+++ week-12/day-4/E_2_Letter_Strings.cpp
@@ -3,6 +3,45 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        
+        map<string,long long> mp;
+        long long ans = 0;
+        for (int i=0; i<n; i++)
+        {
+            string s;
+            cin >> s;
+            string tmp = s;
+            for (char ch='a'; ch<='z'; ch++)
+            {
+                if (ch!=s[0])
+                {
+                    tmp[0] = ch;
+                    ans += mp[tmp];
+                }
+            }
+            tmp = s;
+            for (char ch='a'; ch<='z'; ch++)
+            {
+                if (ch!=s[1])
+                {
+                    tmp[1] = ch;
+                    ans += mp[tmp];
+                }
+            }
+
+            mp[s]++;
+        }
+        
+        cout << ans <<'\n';
+    }
+    
 
 
     return 0;

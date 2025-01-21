8769d77158e8d618d56cd34ec3477012c3699f40
Shuchi28 <seven77bd@gmail.com>
2025-01-21 11:20:15 +0600

    Update D_Friends_and_the_Restaurant.cpp

--- week-12/day-5/D_Friends_and_the_Restaurant.cpp
+++ week-12/day-5/D_Friends_and_the_Restaurant.cpp
@@ -11,8 +11,38 @@ int main()
 
     while (t--)
     {
-        
+        int n;
+        cin >> n;
+        int a[n], b[n], diff[n];
 
+        for (int i=0; i<n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int i=0; i<n; i++)
+        {
+            cin >> b[i];
+            diff[i] = b[i] - a[i];
+        }
+        
+        sort(diff, diff+n);
+        int ans = 0;
+
+        int i=0, j=n-1;
+        while (i<j)
+        {
+            if (diff[i]+diff[j]<0)
+            {
+                i++;
+            }
+            else{
+                ans++;
+                i++;
+                j--;
+            }
+        }
+        
+        cout << ans <<'\n';
     }
     
 

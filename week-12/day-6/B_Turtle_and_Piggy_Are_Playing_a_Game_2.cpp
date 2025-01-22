82ff7dceaea0b00cfc7734554d83897cf73d92cc
Shuchi28 <seven77bd@gmail.com>
2025-01-22 22:38:49 +0600

    Update B_Turtle_and_Piggy_Are_Playing_a_Game_2.cpp

--- week-12/day-6/B_Turtle_and_Piggy_Are_Playing_a_Game_2.cpp
+++ week-12/day-6/B_Turtle_and_Piggy_Are_Playing_a_Game_2.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+        }
+        
+        sort(arr, arr+n);
+
+        int i = n/2;
+        cout << arr[i] <<'\n';
+        
+    }
+    
 
     return 0;
 }

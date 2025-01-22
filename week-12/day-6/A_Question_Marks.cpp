24b417a17d626510389dc0b78aecc988bd257a40
Shuchi28 <seven77bd@gmail.com>
2025-01-23 01:00:32 +0600

    Update A_Question_Marks.cpp

--- week-12/day-6/A_Question_Marks.cpp
+++ week-12/day-6/A_Question_Marks.cpp
@@ -3,6 +3,26 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+        int A = count(s.begin(),s.end(),'A');
+        int B = count(s.begin(),s.end(),'B');
+        int C = count(s.begin(),s.end(),'C');
+        int D = count(s.begin(),s.end(),'D');
+        int q = count(s.begin(),s.end(),'?');
+
+        int ans = min(n,A) + min(n,B) + min(n,C) + min(n,D);
+
+        cout << ans <<'\n';
+    }
+    
 
 
     return 0;

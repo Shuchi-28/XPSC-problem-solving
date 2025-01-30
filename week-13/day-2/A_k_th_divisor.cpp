59d84a0dfa48ebaa33d6bcefc19e21c65d7143cc
Shuchi28 <seven77bd@gmail.com>
2025-01-31 02:37:04 +0600

    Update A_k_th_divisor.cpp

--- week-13/day-2/A_k_th_divisor.cpp
+++ week-13/day-2/A_k_th_divisor.cpp
@@ -3,7 +3,34 @@ using namespace std;
 
 int main()
 {
+    long long int n, k;
+    cin >> n >> k;
 
+    vector<long long> ans;
+    for (long long i=1; i*i<=n; i++)
+    {
+        if (n%i==0)
+        {
+            ans.push_back(i);
+            if ((n/i) != i)
+            {
+                ans.push_back(n/i);
+            }
+        
+        }
+    }
+
+    sort(ans.begin(), ans.end());
+
+    if (ans.size()<k)
+    {
+        cout << "-1\n";
+    }
+    else{
+        cout << ans[k-1] <<"\n";
+    }
+    
+    
 
     return 0;
 }

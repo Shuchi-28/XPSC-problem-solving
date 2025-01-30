b001f3c937b62eee08592f740638833ddd768b91
Shuchi28 <seven77bd@gmail.com>
2025-01-30 17:52:28 +0600

    Update B_Paint_a_Strip.cpp

--- week-13/day-2/B_Paint_a_Strip.cpp
+++ week-13/day-2/B_Paint_a_Strip.cpp
@@ -3,7 +3,48 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+       int n;
+       cin >> n;
+
+       if (n==1)
+       {
+            cout << "1\n";
+       }
+       else if (n==2)
+       {
+            cout << "2\n";
+       }
+       else if (n==3)
+       {
+            cout << "2\n";
+       }
+       else if (n==4)
+       {
+            cout << "2\n";
+       }
+       else{
+            long long int ans = 2, sum = 4;
+            while (sum<n)
+            {
+                ans++;
+                sum++;
+                sum *= 2;
+            }
+            
+            cout << ans <<'\n';
+       }
+       
+       
+    }
+    
 
     return 0;
 }

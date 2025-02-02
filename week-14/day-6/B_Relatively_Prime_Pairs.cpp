2855e7bd98681d5e8209665b8643c1f25e3d52c4
Shuchi28 <seven77bd@gmail.com>
2025-02-02 18:50:30 +0600

    Update B_Relatively_Prime_Pairs.cpp

--- week-14/day-6/B_Relatively_Prime_Pairs.cpp
+++ week-14/day-6/B_Relatively_Prime_Pairs.cpp
@@ -3,7 +3,24 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    long long l, r;
+    cin >> l >> r;
+
+    if ((r-l)%2 == 1)
+    {
+        cout << "YES\n";
+        for (long long i=l; i<=r; i+=2)
+        {
+            cout << i <<" " << i+1 <<'\n';
+        }
+    }
+    else{
+        cout << "NO\n";
+    }
+    
 
     return 0;
 }

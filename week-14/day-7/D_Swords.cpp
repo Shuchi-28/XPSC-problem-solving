c4b0259768b5f2f873df84a7297bac685bb1530d
Shuchi28 <seven77bd@gmail.com>
2025-02-04 22:28:16 +0600

    Update D_Swords.cpp

--- week-14/day-7/D_Swords.cpp
+++ week-14/day-7/D_Swords.cpp
@@ -6,10 +6,33 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n;
-    cin >> n;
+    long long n;
+    cin>>n;
+    long long ara[n+1];
+    long long mx = 0;
 
+    for(int i = 0;i < n;i++){
+        cin>>ara[i];
+        if(ara[i] > mx){
+            mx = ara[i];
+        }
+    }
+    long long z = 0;
+    for(int i = 0;i < n;i++){
+        if(ara[i] == mx){
+            continue;
+        }
+        else{
+            z = __gcd(z,mx-ara[i]);
+        }
+    }
+
+    long long y = 0;
+    for(int i = 0;i < n;i++){
+        y+= (mx-ara[i]);
+    }
     
+    cout<< y/z <<" "<< z <<endl;
     
     return 0;
 }

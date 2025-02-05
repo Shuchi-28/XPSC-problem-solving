bbae571bbb164bdc9e3a1ea8bc2c0470a8b5f5a2
Shuchi28 <seven77bd@gmail.com>
2025-02-06 01:48:54 +0600

    Update D_Coprime.cpp

--- week-15/day-1/D_Coprime.cpp
+++ week-15/day-1/D_Coprime.cpp
@@ -3,7 +3,42 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
 
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> v(1001,0);
+        int ans = -1;
+        for (int i=1; i<=n; i++)
+        {
+            int x;
+            cin >> x;
+            v[x] = i;
+        }
+        for (int i=1; i<=1000; i++)
+        {
+            for (int j=1; j<=1000; j++)
+            {
+                if (v[i] && v[j] && (__gcd(i, j))==1)
+                {
+                    ans = max(ans, v[i]+v[j]);
+                }
+                
+            }
+            
+        }
+        
+        cout << ans <<'\n';
+        
+    }
+    
+    
     return 0;
 }

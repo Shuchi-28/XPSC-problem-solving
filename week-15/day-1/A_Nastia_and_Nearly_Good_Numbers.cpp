cd21764c3ccd4b3228dd3acfdf175a5c64948b4b
Shuchi28 <seven77bd@gmail.com>
2025-02-05 13:59:36 +0600

    Update A_Nastia_and_Nearly_Good_Numbers.cpp

--- week-15/day-1/A_Nastia_and_Nearly_Good_Numbers.cpp
+++ week-15/day-1/A_Nastia_and_Nearly_Good_Numbers.cpp
@@ -3,7 +3,37 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
 
+    while(t--)
+    {
+        long long a, b;
+        cin >> a >> b;
+        
+        if(b==1)
+        {
+            cout << "NO\n";
+        }
+        else{
+            cout << "YES\n";
+            long long x = a;
+            long long y = a*b-a;
+            long long z = a*b;
+            if (x==y)
+            {
+                cout << x <<" " << 2*z-x << " " << 2*z*1LL <<'\n';
+            }
+            else{
+                cout << x << " " << y << " " << z <<'\n';
+            }
+            
+        }
+    }
+    
+    
     return 0;
 }

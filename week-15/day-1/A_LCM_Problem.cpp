459f709071c7a55478eca3fb645d4be1e878bc9d
Shuchi28 <seven77bd@gmail.com>
2025-02-06 12:17:03 +0600

    Update A_LCM_Problem.cpp

--- week-15/day-1/A_LCM_Problem.cpp
+++ week-15/day-1/A_LCM_Problem.cpp
@@ -1,9 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int LCM(int a, int b)
+{
+    return (a/__gcd(a,b))*b;
+}
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
 
+    while(t--)
+    {
+        int l, r;
+        cin >> l >> r;
+
+        if(2LL*l <= r )
+        {
+            cout << l << " " << 2LL*l <<'\n';
+        }
+        else{
+            cout << -1 << " " << -1 <<'\n';
+        }
+        
+    }
+    
+    
     return 0;
 }

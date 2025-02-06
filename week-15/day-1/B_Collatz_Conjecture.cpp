f6a50e0dfd94dab6553d1fecca9e8e9c42d13a08
Shuchi28 <seven77bd@gmail.com>
2025-02-07 02:17:22 +0600

    Update B_Collatz_Conjecture.cpp

--- week-15/day-1/B_Collatz_Conjecture.cpp
+++ week-15/day-1/B_Collatz_Conjecture.cpp
@@ -3,7 +3,48 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    int t;
+    cin >> t;
 
+    while(t--)
+    {
+        int x, y, k;
+        cin >> x >> y >> k;
+
+        while(x>1)
+        {
+            int p = y-(x%y);
+            if(p>=k)
+            {
+                x += k;
+                k = 0;
+            }
+            else{
+                k -= p;
+                x += p;
+            }
+            while (x%y == 0)
+            {
+                x /=y;
+            }
+            if(k<=0) break;
+        }
+        if (k>0)
+        {
+            k = k%(y-1);
+            x += k;
+            while (x%y == 0)
+            {
+                x = x/y;
+            }
+        }
+        
+        cout << x <<'\n';
+    }
+    
+    
     return 0;
 }

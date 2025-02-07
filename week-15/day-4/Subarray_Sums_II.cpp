5f54579a9c53449ad0b99f15532e57f1d477d16d
Shuchi28 <seven77bd@gmail.com>
2025-02-07 20:29:07 +0600

    Update Subarray_Sums_II.cpp

--- week-15/day-4/Subarray_Sums_II.cpp
+++ week-15/day-4/Subarray_Sums_II.cpp
@@ -3,7 +3,29 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
+    long long n, x, sum = 0, ans=0;
+    cin >> n >> x;
 
+    map<long long, long long> mp;
+    mp[0] = 1;
+
+    vector<int> v(n);
+    for (int i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    for (int i=0; i<n; i++)
+    {
+        sum += v[i];
+        ans += mp[sum-x];
+        mp[sum]++;
+    }
+    
+    cout << ans;
+
+    
     return 0;
 }

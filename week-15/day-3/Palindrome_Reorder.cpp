d35e2bd0e7de9aa6c67b97dba9364eb87086becc
Shuchi28 <seven77bd@gmail.com>
2025-02-07 12:16:02 +0600

    Update Palindrome_Reorder.cpp

--- week-15/day-3/Palindrome_Reorder.cpp
+++ week-15/day-3/Palindrome_Reorder.cpp
@@ -6,6 +6,44 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    string s, c, ans="";
+    cin >> s;
+
+    map<char, int> mp;
+    for (int i=0; i<s.size(); i++)
+    {
+        mp[s[i]]++;
+    }
+
+    int flag = 0;
+    for (auto it: mp)
+    {
+        if (it.second%2==1)
+        {
+            flag++;
+            c = it.first;
+        }
+    }
+    if (flag>1)
+    {
+        cout << "NO SOLUTION";
+    }
+    else{
+        for (auto it: mp)
+        {
+            for (int i=0; i<it.second/2; i++)
+            {
+                ans.push_back(it.first);
+            }
+        }
+        s = ans;
+        s += c;
+        reverse(ans.begin(), ans.end());
+        s += ans;
+        
+        cout << s;
+        
+    }
     
     
     return 0;

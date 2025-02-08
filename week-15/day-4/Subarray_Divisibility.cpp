12220ca8cf5c5870e1368ba27e516f9aede7d236
Shuchi28 <seven77bd@gmail.com>
2025-02-08 13:04:15 +0600

    Update Subarray_Divisibility.cpp

--- week-15/day-4/Subarray_Divisibility.cpp
+++ week-15/day-4/Subarray_Divisibility.cpp
@@ -3,7 +3,27 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
 
+    vector<int> v(n);
+    for (int i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    
+    long long sum = 0, cnt = 0;
+    map<long long, int> mp;
+    mp[0]++;
+    for (int i = 0; i<n; i++)
+    {
+        sum += v[i]%n;
+        sum = (sum+n)%n;
+        cnt += mp[sum];
+        mp[sum]++;
+    }
+    
+    cout << cnt;
 
     return 0;
 }

a927a0f125c53d5736c8fdb2f1b4432bd304c119
Shuchi28 <seven77bd@gmail.com>
2025-02-08 17:24:04 +0600

    Update Subarray_Distinct_Values.cpp

--- week-15/day-4/Subarray_Distinct_Values.cpp
+++ week-15/day-4/Subarray_Distinct_Values.cpp
@@ -3,6 +3,33 @@ using namespace std;
 
 int main()
 {
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+
+    for (int i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    
+    long long ans = 0;
+    int j = 0;
+    map<int, int> mp;
+    for (int i=0; i<n; i++)
+    {
+        while (j<n && (mp.size()<k || mp.count(v[j])>0))
+        {
+            mp[v[j]]++;
+            j++;
+        }
+        ans += j-i;
+        mp[v[i]]--;
+        if (mp[v[i]]==0)
+            mp.erase(v[i]);
+        
+    }
+    
+    cout << ans;
 
 
     return 0;

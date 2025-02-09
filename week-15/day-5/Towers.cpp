a43a4e5cb686c72f28d7ce43b6cd5a561cf5ada8
Shuchi28 <seven77bd@gmail.com>
2025-02-09 23:46:20 +0600

    Update Towers.cpp

--- week-15/day-5/Towers.cpp
+++ week-15/day-5/Towers.cpp
@@ -3,7 +3,25 @@ using namespace std;
 
 int main()
 {
-
-
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    long long n, temp;
+    cin >> n;
+    vector<int> v;
+    for (int i=0; i<n; i++)
+    {
+        cin >> temp;
+        auto it = upper_bound(v.begin(),v.end(), temp);
+        if (it==v.end())
+        {
+            v.push_back(temp);
+        }
+        else{
+            *it = temp;
+        }
+    }
+    
+    cout << v.size() <<endl;
     return 0;
-}
+} 

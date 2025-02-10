1af39caa859ee4489e1cc273d7c02eeb77db73e3
Shuchi28 <seven77bd@gmail.com>
2025-02-10 12:32:18 +0600

    Update Collecting_Numbers.cpp

--- week-15/day-6/Collecting_Numbers.cpp
+++ week-15/day-6/Collecting_Numbers.cpp
@@ -6,8 +6,29 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
+    int n;
+    cin >> n;
+    vector<pair<int, int>> v(n);
 
+    for (int i=0; i<n; i++)
+    {
+        cin >> v[i].first;
+        v[i].second = i+1;
+    }
+    
+    sort(v.begin(), v.end());
+
+    int round = 1;
+    for (int i=0; i<n-1; i++)
+    {
+        if (v[i].second>v[i+1].second)
+        {
+            round++;
+        }
+        
+    }
+    
+    cout << round;
     
     return 0;
 }

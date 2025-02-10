58c67202cbf990613f85f4d67864ee5ed6d0d187
Shuchi28 <seven77bd@gmail.com>
2025-02-10 22:28:49 +0600

    Update B_Closest_to_the_Left.cpp

--- week-15/day-7/B_Closest_to_the_Left.cpp
+++ week-15/day-7/B_Closest_to_the_Left.cpp
@@ -6,6 +6,36 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
+    int n, q;
+    cin >> n >> q;
+    int arr[n];
+    for (int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    while (q--)
+    {
+        int x;
+        cin >> x;
+        int l=0, r=n-1, ans = -1;
+
+        while (l<=r)
+        {
+            int mid =(r+l)/2;
+            if (x>=arr[mid])
+            {
+                ans = mid;
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+        }
+        
+        cout << (ans+1) <<'\n';
+    
+    }
 
     return 0;
 }

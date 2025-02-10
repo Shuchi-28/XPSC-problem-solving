39859b4195e4fbff64dbae750e1f39e38b53ecb5
Shuchi28 <seven77bd@gmail.com>
2025-02-10 22:30:32 +0600

    Update C_Closest_to_the_Right.cpp

--- week-15/day-7/C_Closest_to_the_Right.cpp
+++ week-15/day-7/C_Closest_to_the_Right.cpp
@@ -6,6 +6,36 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
+    int n, q;
+    cin >> n >> q;
+    int arr[n];
+    for (int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    while (q--)
+    {
+        int x;
+        cin >> x;
+        int l=0, r=n-1, ans = n;
+
+        while (l<=r)
+        {
+            int mid =(r+l)/2;
+            if (x<=arr[mid])
+            {
+                ans = mid;
+                r = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        
+        cout << ans+1 <<'\n';
+    
+    }
 
     return 0;
 }

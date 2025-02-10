5df8261f14e948c3d3a08327790429a807bca90c
Shuchi28 <seven77bd@gmail.com>
2025-02-10 20:44:12 +0600

    Update A_Binary_Search.cpp

--- week-15/day-7/A_Binary_Search.cpp
+++ week-15/day-7/A_Binary_Search.cpp
@@ -6,6 +6,42 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
+    int n,q;
+    cin >> n >> q;
+    int arr[n];
+    for (int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr,arr+n);
+
+    while (q--)
+    {
+        int x;
+        cin >> x;
+        int l=0, r=n-1;
+        bool flag = false;
+
+        while (l<=r)
+        {
+            int mid =(r+l)/2;
+            if (arr[mid] == x)
+            {
+                flag = true;
+                break;
+            }
+            if (x>arr[mid])
+            {
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+        }
+        if (flag==true) cout << "YES\n";
+        else cout << "NO\n";
+    
+    }
 
     return 0;
 }

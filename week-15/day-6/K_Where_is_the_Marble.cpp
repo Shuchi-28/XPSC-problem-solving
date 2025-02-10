24e8dd145b385b68cd3569eba6b79cbf49b50922
Shuchi28 <seven77bd@gmail.com>
2025-02-10 16:19:07 +0600

    Update K_Where_is_the_Marble.cpp

--- week-15/day-6/K_Where_is_the_Marble.cpp
+++ week-15/day-6/K_Where_is_the_Marble.cpp
@@ -6,6 +6,36 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int n, q, caseno = 0;
+    while (cin >> n >> q && (n != 0 || q != 0)) 
+    {
+        caseno++;
+        vector<int> marbles(n);
+        for (int i = 0; i < n; i++) 
+        {
+            cin >> marbles[i];
+        }
+        
+        sort(marbles.begin(), marbles.end());
+        
+        cout << "CASE# " << caseno << ":" << endl;
+        
+        while (q--) 
+        {
+            int query;
+            cin >> query;
+            
+            auto it = lower_bound(marbles.begin(), marbles.end(), query);
+            
+            if (it != marbles.end() && *it == query) 
+            {
+                cout << query << " found at " << (it - marbles.begin() + 1) << endl;
+            } 
+            else {
+                cout << query << " not found" << endl;
+            }
+        }
+    }
     
 
     

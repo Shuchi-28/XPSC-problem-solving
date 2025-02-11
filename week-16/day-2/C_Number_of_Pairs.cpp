a407980726dd0a51bbcbff3bd8022c6115124efc
Shuchi28 <seven77bd@gmail.com>
2025-02-12 01:32:31 +0600

    Update C_Number_of_Pairs.cpp

--- week-16/day-2/C_Number_of_Pairs.cpp
+++ week-16/day-2/C_Number_of_Pairs.cpp
@@ -6,6 +6,33 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n, l, r;
+        cin >> n >> l >> r;
+
+        vector<long long> v(n);
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
+        
+        sort(v.begin(), v.end());
+        long long ans = 0;
+        for (int i=0; i<n; i++)
+        {
+            int indx = lower_bound(v.begin()+i+1, v.end(), l-v[i]) - v.begin();
+            int indx2 = upper_bound(v.begin()+i+1, v.end(), r-v[i]) - v.begin();
+
+            ans += indx2-indx;
+        }
+        
+        cout << ans <<'\n';
+    }
+
 
     return 0;
 }

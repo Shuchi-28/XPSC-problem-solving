185d1a0a84c8bea13feb56eb473842bd5b4162cd
Shuchi28 <seven77bd@gmail.com>
2025-02-11 18:36:06 +0600

    Update 10611_The_Playboy_Chimp.cpp

--- week-16/day-2/10611_The_Playboy_Chimp.cpp
+++ week-16/day-2/10611_The_Playboy_Chimp.cpp
@@ -4,8 +4,40 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(NULL);
 
+    int n;
+    cin >> n;
+    vector<int> v(n);
 
+    for (int i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    
+    sort(v.begin(), v.end());
+
+    int q;
+    cin >> q;
+
+    for (int i = 0; i < q; i++)
+    {
+        int x;
+        cin >> x;
+
+        auto lowit = lower_bound(v.begin(), v.end(), x);
+        auto upit = upper_bound(v.begin(), v.end(), x);
+        if (lowit == v.begin())
+            cout << "X ";
+        else 
+            cout << *(--lowit) <<" ";
+            
+        if (upit == v.end())
+            cout << "X\n";
+        else 
+            cout << *upit <<'\n';
+        
+    }
+    
     return 0;
 }

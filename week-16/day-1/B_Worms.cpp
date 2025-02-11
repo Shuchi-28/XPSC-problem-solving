a18ce5d8b97a212889a9c3c4097427e064348a0f
Shuchi28 <seven77bd@gmail.com>
2025-02-11 18:27:59 +0600

    Update B_Worms.cpp

--- week-16/day-1/B_Worms.cpp
+++ week-16/day-1/B_Worms.cpp
@@ -3,7 +3,29 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i=0; i<n; i++)
+    {
+        cin >> v[i];
+        if(i!=0)
+            v[i] += v[i-1];
+    }
+    
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int x;
+        cin >> x;
+        auto it = lower_bound(v.begin(), v.end(), x);
 
-
+        cout << it-v.begin()+1 <<'\n';
+    }
+    
     return 0;
 }

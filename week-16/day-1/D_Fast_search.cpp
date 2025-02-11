73c26ab9f6e5f94d36d4bea0eabb3b4dff24e73b
Shuchi28 <seven77bd@gmail.com>
2025-02-11 13:31:33 +0600

    Update D_Fast_search.cpp

--- week-16/day-1/D_Fast_search.cpp
+++ week-16/day-1/D_Fast_search.cpp
@@ -3,7 +3,32 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
 
+    vector<int> v(n);
+    for (int i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int l, r;
+        cin >> l >> r;
 
+        auto lowit = lower_bound(v.begin(), v.end(), l);
+        auto upit = upper_bound(v.begin(), v.end(), r);
+
+        cout << (upit-lowit) <<" ";
+        
+    }
+    
+        
     return 0;
 }

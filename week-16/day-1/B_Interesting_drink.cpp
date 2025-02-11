238bab6f2d5f1cec26d1cf0fa6b8b4ad2fc3679b
Shuchi28 <seven77bd@gmail.com>
2025-02-11 14:05:30 +0600

    Update B_Interesting_drink.cpp

--- week-16/day-1/B_Interesting_drink.cpp
+++ week-16/day-1/B_Interesting_drink.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    vector<int> v(n+1);
+    for (int i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    
+    sort(v.begin(), v.end());
 
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int x;
+        cin >> x;
 
+        auto upit = upper_bound(v.begin(), v.end(), x);
+        cout << upit-v.begin()-1 <<'\n';
+    }
+    
+        
     return 0;
 }

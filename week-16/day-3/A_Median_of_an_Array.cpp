a894b629826cc2f5fd32ba01286d7b42b312ea50
Shuchi28 <seven77bd@gmail.com>
2025-02-12 18:46:00 +0600

    Update A_Median_of_an_Array.cpp

--- week-16/day-3/A_Median_of_an_Array.cpp
+++ week-16/day-3/A_Median_of_an_Array.cpp
@@ -1,3 +1,4 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -6,6 +7,33 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, cnt=0;
+        cin >> n;
+
+        vector<int> v(n);
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+
+        int x = (n-1)/2;
+        int val = v[x];
+        for (int i=x; i<n; i++)
+        {
+            if (val==v[i])
+            {
+                cnt++;
+            }
+            
+        }
+        
+        cout << cnt <<'\n';
+    }
     
 
     return 0;

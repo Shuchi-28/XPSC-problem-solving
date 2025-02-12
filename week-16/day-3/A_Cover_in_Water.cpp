6c7afc50b6b19cf34ec2315fd1fe499aac2ccb97
Shuchi28 <seven77bd@gmail.com>
2025-02-12 16:47:18 +0600

    Update A_Cover_in_Water.cpp

--- week-16/day-3/A_Cover_in_Water.cpp
+++ week-16/day-3/A_Cover_in_Water.cpp
@@ -6,6 +6,38 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, flag = 0;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        int a = count(s.begin(), s.end(),'#');
+        if (n==a)
+        {
+            cout << "0\n";
+            continue;
+        }
+
+        for (int i=0; i<n-2; i++)
+        {
+            if (s[i]=='.' && s[i+1]=='.' && s[i+2]=='.')
+            {
+                flag = 1;
+            }
+        }
+        
+        if (flag)
+        {
+            cout << 2 <<"\n";
+        }
+        else{
+            cout << count(s.begin(), s.end(),'.') <<"\n";
+        }
+    }
     
 
     return 0;

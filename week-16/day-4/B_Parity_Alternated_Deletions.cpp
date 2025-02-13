0058c38139b708f1bc0f5599f5dcd04b805c35e0
Shuchi28 <seven77bd@gmail.com>
2025-02-14 03:20:20 +0600

    Update B_Parity_Alternated_Deletions.cpp

--- week-16/day-4/B_Parity_Alternated_Deletions.cpp
+++ week-16/day-4/B_Parity_Alternated_Deletions.cpp
@@ -3,7 +3,36 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+
+    int n, ans = 0;
+    cin >> n;
+    vector<vector<int>> a(2);
+
+    for (int i=0; i<n; i++)
+    {
+        int x;
+        cin >> x;
+        a[x%2].push_back(x);
+    }
+        
+    sort(a[0].rbegin(), a[0].rend());
+    sort(a[1].rbegin(), a[1].rend());
+
+    int k = min(a[0].size(), a[1].size());
+        
+    for (int x: {0,1})
+    {
+        for (int i=k+1; i<a[x].size(); i++)
+        {
+            ans += a[x][i];
+        }
+        
+    }
+    cout << ans;
+    
 
     return 0;
 }

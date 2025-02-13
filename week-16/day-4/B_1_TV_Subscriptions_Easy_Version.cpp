6e1f65ac1ca6770eb53c142d65f42c7309199b56
Shuchi28 <seven77bd@gmail.com>
2025-02-14 00:53:42 +0600

    Update B_1_TV_Subscriptions_Easy_Version.cpp

--- week-16/day-4/B_1_TV_Subscriptions_Easy_Version.cpp
+++ week-16/day-4/B_1_TV_Subscriptions_Easy_Version.cpp
@@ -3,7 +3,49 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, k, d;
+        cin >> n >> k >> d;
+        
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        int l= 0, r= 0, ans=INT_MAX;
+        map<int, int> mp;
+
+        while (r<n)
+        {
+            mp[arr[r]]++;
+            if (r-l+1 == d)
+            {
+                int a = mp.size();
+                ans = min(ans, a);
+                mp[arr[l]]--;
+                if(mp[arr[l]]==0)
+                    mp.erase(arr[l]);
+                l++;
+                r++;
+
+            }
+            else{
+                r++;
+            }
+            
+        }
+        
+        cout << ans <<'\n';
+    }
+    
 
     return 0;
 }

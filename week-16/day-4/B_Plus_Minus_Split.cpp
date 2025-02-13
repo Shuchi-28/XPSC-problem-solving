7f12f260b5873b750f0dbae44e142ca0d7b5708f
Shuchi28 <seven77bd@gmail.com>
2025-02-13 19:20:15 +0600

    Update B_Plus_Minus_Split.cpp

--- week-16/day-4/B_Plus_Minus_Split.cpp
+++ week-16/day-4/B_Plus_Minus_Split.cpp
@@ -3,7 +3,27 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        int a = count(s.begin(), s.end(),'-');
+        int b = count(s.begin(), s.end(),'+');
+
+        cout << abs(a-b) <<'\n';
+
+    }
+    
 
     return 0;
 }

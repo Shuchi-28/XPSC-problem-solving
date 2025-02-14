40811fdeaf657dc4443bf5a3b5bdd201f1e780a4
Shuchi28 <seven77bd@gmail.com>
2025-02-14 19:44:27 +0600

    Update B_Maximums.cpp

--- week-16/day-5/B_Maximums.cpp
+++ week-16/day-5/B_Maximums.cpp
@@ -3,7 +3,25 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+    
+    vector<int> b(n);
+    for (int i=0; i<n; i++)
+    {
+        cin >> b[i];
+    }
+    
+    int x = 0;
 
+    for (int i=0; i<n; i++)
+    {
+        int ai = b[i]+x;
+        cout << ai <<" ";
+
+        x = max(x, ai);
+    }
+    
 
     return 0;
 }

bf1f4035ff67117dae39a523bf0e70d39b52df2f
Shuchi28 <seven77bd@gmail.com>
2025-02-14 11:31:22 +0600

    Update A_Stickogon.cpp

--- week-16/day-3/A_Stickogon.cpp
+++ week-16/day-3/A_Stickogon.cpp
@@ -6,6 +6,30 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, cnt=0;
+        cin >> n;
+
+        map<int, int> mp;
+        for (int i=0; i<n; i++)
+        {
+            int x;
+            cin >> x;
+            mp[x]++;
+        }
+        
+        for (auto a: mp)
+        {
+            if(a.second>2)
+                cnt+=a.second/3;
+        }
+        
+        cout << cnt <<'\n';
+    }
     
 
     return 0;

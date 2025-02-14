246459b99389654342ccbef11f7d458c9fa24a12
Shuchi28 <seven77bd@gmail.com>
2025-02-14 11:31:58 +0600

    Update A_Yogurt_Sale.cpp

--- week-16/day-3/A_Yogurt_Sale.cpp
+++ week-16/day-3/A_Yogurt_Sale.cpp
@@ -6,7 +6,27 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, a, b;
+        cin >> n >> a >> b;
+
+        int x = min(2*a, b);
+        if (n%2!=0)
+        {
+            n /=2;
+            x = (n*x)+a;
+        }
+        else{
+            n /= 2;
+            x = (n*x);
+        }
+        
+        cout << x <<'\n';
+
+    }
 
     return 0;
 }

7c7068f8ea6e5a5bd46743023c6627221e7b82db
Shuchi28 <seven77bd@gmail.com>
2025-02-14 12:09:42 +0600

    Update I_Maximal_Continuous_Rest.cpp

--- week-16/day-4/I_Maximal_Continuous_Rest.cpp
+++ week-16/day-4/I_Maximal_Continuous_Rest.cpp
@@ -3,7 +3,40 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    int n;
+    cin >> n;
+    int ans = 0, cnt=0;
+    int arr[n];
+
+    for (int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i=0; i<n; i++)
+    {
+        if(arr[i]==1)
+        {
+            cnt++;
+            ans = max(ans, cnt);
+        }
+        else
+            cnt = 0;
+    }
+    for (int i=0; i<n; i++)
+    {
+        if(arr[i]==1)
+        {
+            cnt++;
+            ans = max(ans, cnt);
+        }
+        else
+            cnt = 0;
+    }
+
+    cout << ans;
 
     return 0;
 }

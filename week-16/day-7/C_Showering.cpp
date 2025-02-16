4392565d72576865db0f4daf97f6aeb8f9e46590
Shuchi28 <seven77bd@gmail.com>
2025-02-16 18:47:31 +0600

    Update C_Showering.cpp

--- week-16/day-7/C_Showering.cpp
+++ week-16/day-7/C_Showering.cpp
@@ -11,7 +11,32 @@ int main()
 
     while(t--)
     {
+        int n, s, m;
+        cin >> n >> s >> m;
+
+        vector<int> v;
+        v.push_back(0);
+        for (int i=0; i<n; i++)
+        {
+            int l, r;
+            cin >> l >> r;
+            v.push_back(l);
+            v.push_back(r);
+        }
+        v.push_back(m);
+
+        int flag = 0;
+        for (int i=0; i<v.size(); i+=2)
+        {
+            if ((v[i+1]-v[i])>=s)
+                flag = 1;
+            
+        }
         
+        if(flag==1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
 
     }
 

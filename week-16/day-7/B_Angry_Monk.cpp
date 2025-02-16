5e1487bb099199f7da6549e09407651607380880
Shuchi28 <seven77bd@gmail.com>
2025-02-16 22:24:00 +0600

    Update B_Angry_Monk.cpp

--- week-16/day-7/B_Angry_Monk.cpp
+++ week-16/day-7/B_Angry_Monk.cpp
@@ -11,16 +11,22 @@ int main()
 
     while(t--)
     {
-        int n;
-        cin >> n;
+        int n, k, ans=0;;
+        cin >> k >> n;
 
         vector<int> v(n);
         for (int i=0; i<n; i++)
         {
             cin >> v[i];
         }
-        
+        sort(v.begin(), v.end());
 
+        for (int i=0; i<n-1; i++)
+        {
+            ans += v[i]+v[i]-1;
+        }
+        
+        cout << ans <<endl;
     }
 
     return 0;

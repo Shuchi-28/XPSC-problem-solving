a92abdfe1d7c18ac87b6adcca9a4c84a5faf06f8
Shuchi28 <seven77bd@gmail.com>
2025-02-16 11:17:49 +0600

    Update B_Summation_Game.cpp

--- week-16/day-5/B_Summation_Game.cpp
+++ week-16/day-5/B_Summation_Game.cpp
@@ -3,6 +3,43 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, k, x;
+        cin >> n >> k >> x;
+
+        vector<int> v(n);
+        vector<int> pre(n+1);
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
+        
+        sort(v.begin(), v.end());
+        for (int i=1; i<=n; i++)
+        {
+            pre[i] = pre[i-1]+v[i-1];
+        }
+        
+        int ans = INT_MIN;
+        for (int i=0; i<=k; i++)
+        {
+            int remain = n-i;
+            int neg = min(remain, x);
+
+            int ans1 = pre[remain]- 2*(pre[remain]-pre[remain-neg]);
+            ans = max(ans, ans1);
+        }
+        
+        cout << ans <<'\n';
+    }
+    
 
 
     return 0;

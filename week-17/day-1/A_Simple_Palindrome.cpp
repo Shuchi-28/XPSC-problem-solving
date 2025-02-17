f791fa3fed72a69d6a01c3dd7c5a7c38d14d7d92
Shuchi28 <seven77bd@gmail.com>
2025-02-17 16:11:38 +0600

    Update A_Simple_Palindrome.cpp

--- week-17/day-1/A_Simple_Palindrome.cpp
+++ week-17/day-1/A_Simple_Palindrome.cpp
@@ -6,7 +6,30 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int t;
+    cin >> t;
 
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s = "", vowels = "aeiou";
+        int over = n/5;
+        int mod = n%5;
+        while (over--)
+        {
+            s += vowels;
+        }
+        for (int i=0; i<mod; i++)
+        {
+            s += vowels[i];
+        }
+        
+        sort(s.begin(), s.end());
+        cout << s <<endl;
+    }
+
+    
     return 0;
 }

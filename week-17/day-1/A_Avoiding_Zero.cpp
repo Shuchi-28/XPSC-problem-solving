15a6168d2bf6583d8f4ccaa24ffdb7236ffafc9c
Shuchi28 <seven77bd@gmail.com>
2025-02-17 19:31:05 +0600

    Update A_Avoiding_Zero.cpp

--- week-17/day-1/A_Avoiding_Zero.cpp
+++ week-17/day-1/A_Avoiding_Zero.cpp
@@ -6,6 +6,67 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, sum=0, p=0, nn=0;
+        cin >> n;
+        vector<int> v(n);
+        vector<int> pos;
+        vector<int> neg;
+        vector<int> zero;
+
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+            sum += v[i];
+        }
+        for (int i=0; i<n; i++)
+        {
+            if(v[i]>0)
+            {
+                pos.push_back(v[i]);
+                p += v[i];
+            }
+            if(v[i]<0)
+            {
+                neg.push_back(v[i]);
+                nn += v[i];
+            }
+            if(v[i]==0)
+                zero.push_back(v[i]);
+        }
+        if (sum == 0)
+        {
+            cout << "NO\n";
+        }
+        else{
+            cout << "YES\n";
+            if (p>abs(nn))
+            {
+                for(int x: pos)
+                    cout << x << " ";
+                for(int x: neg)
+                    cout << x << " ";
+                for(int x: zero)
+                    cout << x << " ";
+            }
+            else if (p<abs(nn))
+            {
+                for(int x: neg)
+                    cout << x << " ";
+                for(int x: pos)
+                    cout << x << " ";
+                for(int x: zero)
+                    cout << x << " ";
+            }
+            
+            cout <<'\n';
+        }
+        
+    }
     
 
     return 0;

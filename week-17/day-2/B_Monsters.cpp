ffeb6ddaf859a805d617528e52c26b62e7943db0
Shuchi28 <seven77bd@gmail.com>
2025-02-18 23:45:31 +0600

    Update B_Monsters.cpp

--- week-17/day-2/B_Monsters.cpp
+++ week-17/day-2/B_Monsters.cpp
@@ -11,7 +11,23 @@ int main()
 
     while (t--)
     {
+        int n, k;
+        cin >> n >> k;
+
+        vector<pair<int, int>> v(n);
+        for (int i=0; i<n; i++)
+        {
+            int x;
+            cin >> x;
+            v[i] = {-1*(x%k==0? k:x%k), i+1};
+        }
         
+        sort(v.begin(), v.end());
+        for (auto a: v)
+        {
+            cout << a.second << " ";
+        }
+        cout << '\n';
         
     }
     

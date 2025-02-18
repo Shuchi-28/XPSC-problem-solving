2e9e0f022e893b4055a6ce4dfd294cf7a88a85ae
Shuchi28 <seven77bd@gmail.com>
2025-02-18 22:15:02 +0600

    Update B_Mark_the_Dust_Sweeper.cpp

--- week-17/day-2/B_Mark_the_Dust_Sweeper.cpp
+++ week-17/day-2/B_Mark_the_Dust_Sweeper.cpp
@@ -4,14 +4,43 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(NULL);
 
     int t;
     cin >> t;
-
     while (t--)
     {
+        int n, once=0, zeros=0, Lzero=0;
+        cin >> n;
+        vector<int> v(n);
+        long long sum = 0;
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+            sum +=  v[i];
+            if (v[i]==0)
+            {
+                zeros++;
+            }
+        }
+        sum -= v[n-1];
+        for (int i=0; i<n; i++)
+        {
+            if (v[i]!=0)
+            {
+                once=1;
+            }
+            if (once==0 && v[i]==0)
+            {
+                Lzero++;
+            }
+        }
         
+        long long ans = (zeros-Lzero)+sum;
+        if(v[n-1]==0 && ans!=0)
+            ans--;
+
+        cout << ans <<'\n';
     }
     
 

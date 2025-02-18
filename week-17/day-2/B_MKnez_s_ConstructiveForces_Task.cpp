eb237debae8f220a7cfcb04e5782e73cff9d4808
Shuchi28 <seven77bd@gmail.com>
2025-02-18 21:26:45 +0600

    Update B_MKnez_s_ConstructiveForces_Task.cpp

--- week-17/day-2/B_MKnez_s_ConstructiveForces_Task.cpp
+++ week-17/day-2/B_MKnez_s_ConstructiveForces_Task.cpp
@@ -11,6 +11,32 @@ int main()
 
     while (t--)
     {
+        int n;
+        cin >> n;
+        if (n%2 == 0)
+        {
+            cout << "YES\n";
+            for (int i=0; i<n/2; i++)
+            {
+                cout << 1 << " " << -1 << " ";
+            }
+            cout << '\n';
+        }
+        else{
+            if(n == 3)
+                cout << "NO\n";
+            else{
+                cout << "YES\n";
+                for (int i=1; i<=n; i++)
+                {
+                    if(i%2!=0)
+                        cout << (n/2)-1 <<" ";
+                    else
+                        cout << -n/2 << " ";
+                }
+                cout << '\n';
+            }
+        }
         
         
     }

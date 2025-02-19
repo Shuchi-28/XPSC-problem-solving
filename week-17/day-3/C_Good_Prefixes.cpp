60d9645e4202981696110d6acc3d05e76a67e999
Shuchi28 <seven77bd@gmail.com>
2025-02-20 01:43:59 +0600

    Update C_Good_Prefixes.cpp

--- week-17/day-3/C_Good_Prefixes.cpp
+++ week-17/day-3/C_Good_Prefixes.cpp
@@ -4,15 +4,35 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(NULL);
 
     int t;
     cin >> t;
-
     while (t--)
     {
+        int n, cnt=0;
+        cin >> n;
+        vector<long long> a(n);
+        for (int i=0; i<n; i++)
+        {
+            cin >> a[i];
+        }
+        int mx = INT_MIN;
+        long long sum = 0;
+
+        for (int i=0; i<n; i++)
+        {
+            sum += a[i];
+            if (a[i]>mx)
+                mx = a[i];
+            if (2*mx == sum)
+            {
+                cnt++;
+            }
+        }
         
-        
+        cout << cnt <<'\n';
+
     }
     
 

c2f55782ef255766c0eb703346ec661683bff6e2
Shuchi28 <seven77bd@gmail.com>
2025-02-20 19:45:02 +0600

    Update B_Good_Kid.cpp

--- week-17/day-4/B_Good_Kid.cpp
+++ week-17/day-4/B_Good_Kid.cpp
@@ -3,6 +3,31 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, ans = 1, flag=0;
+        cin >> n;
+
+        vector<int> v(n);
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
+        
+        sort(v.begin(), v.end());
+        v[0] = v[0]+1;
+        for (int i=0; i<n; i++)
+        {
+            ans *= v[i];
+        }
+        
+        cout << ans <<'\n';
+        
+    }
+    
 
 
     return 0;

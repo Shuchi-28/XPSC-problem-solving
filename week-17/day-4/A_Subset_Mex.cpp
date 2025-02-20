eece3ab7c10c4e29a4f3bd45be0365ada6cad353
Shuchi28 <seven77bd@gmail.com>
2025-02-20 21:00:32 +0600

    Update A_Subset_Mex.cpp

--- week-17/day-4/A_Subset_Mex.cpp
+++ week-17/day-4/A_Subset_Mex.cpp
@@ -3,7 +3,51 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
 
+    while (t--)
+    {
+        int n, mexA, mexB;
+        cin >> n;
 
+        vector<int> cnt(105,0);
+        for (int i=0; i<n; i++)
+        {
+            int x;
+            cin >> x;
+            cnt[x]++;
+        }
+        for (int i=0; i<105; i++)
+        {
+            if(cnt[i]==0)
+            {
+                mexA = i;
+                break;
+            }
+            else
+            {
+                cnt[i]--;
+            }
+            
+        }
+        for (int i=0; i<105; i++)
+        {
+            if(cnt[i]==0)
+            {
+                mexB = i;
+                break;
+            }
+            else
+            {
+                cnt[i]--;
+            }
+            
+        }
+        
+        cout << mexA+mexB <<'\n';
+    
+    }
+    
     return 0;
 }

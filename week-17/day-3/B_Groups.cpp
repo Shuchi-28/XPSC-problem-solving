ac65c3a5dd37efcabdb5d4265e1d0be90f86c8c9
Shuchi28 <seven77bd@gmail.com>
2025-02-20 14:05:00 +0600

    Update B_Groups.cpp

--- week-17/day-3/B_Groups.cpp
+++ week-17/day-3/B_Groups.cpp
@@ -11,7 +11,49 @@ int main()
 
     while (t--)
     {
+        int n, flag=0;
+        cin >> n;
+
+        int arr[n][5];
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<5; j++)
+            {
+                cin >> arr[i][j];
+            }
+            
+        }
+
+        for (int d1=0; d1<5; d1++)
+        {
+            for (int d2=d1+1; d2<5; d2++)
+            {
+                int cntd1=0;
+                int cntd2=0;
+                int common=0;
+                for (int i=0; i<n; i++)
+                {
+                    if (arr[i][d1]==1)
+                        cntd1++;
+                    
+                    if (arr[i][d2]==1)
+                        cntd2++;
+                    if(arr[i][d1] && arr[i][d2])
+                        common++;
+                }
+                if(cntd1>=n/2 && cntd2>=n/2 && (cntd1+cntd2-common == n))
+                {
+                    flag = 1;
+                }
+
+            }
+        }
         
+        if(flag)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+
         
     }
     

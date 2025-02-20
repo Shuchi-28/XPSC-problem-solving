c9a9b4f6deeac39e78e092396cb40f1cdae3288b
Shuchi28 <seven77bd@gmail.com>
2025-02-20 15:02:42 +0600

    Update E_Negatives_and_Positives.cpp

--- week-17/day-3/E_Negatives_and_Positives.cpp
+++ week-17/day-3/E_Negatives_and_Positives.cpp
@@ -11,8 +11,26 @@ int main()
 
     while (t--)
     {
-        
-        
+        long long n, sum=0, neg=0, mn=INT_MAX;
+        cin >> n;
+
+        vector<int> v(n);
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+            sum += abs(v[i]);
+            if(mn>abs(v[i]))
+                mn = abs(v[i]);
+            if(v[i]<0)
+                neg++;
+        }
+        if(neg%2 == 0)
+            cout << sum <<'\n';
+        else
+        {
+            cout << sum-(2*mn) <<'\n';
+        }
+
     }
     
 

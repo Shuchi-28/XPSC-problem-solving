fa093fe8c85ce742cdfffc038302b51fb39b379c
Shuchi28 <seven77bd@gmail.com>
2025-02-21 22:22:52 +0600

    Update B_Ugu.cpp

--- week-17/day-5/B_Ugu.cpp
+++ week-17/day-5/B_Ugu.cpp
@@ -3,6 +3,48 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
+    
+    while (t--)
+    {
+        int n, segment=0;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        if (is_sorted(s.begin(), s.end()))
+        {
+            cout << "0\n";
+            continue;
+        }
+
+        for (int i=0; i<n; i++)
+        {
+            if (s[i]=='1' &&(i==0 || s[i-1]=='0') )
+            {
+                segment++;
+            }
+        }
+        
+        if (segment==1 && s[n-1]=='0')
+        {
+            cout <<"1\n";
+        }
+        else if (segment>1 && s[n-1]=='1')
+        {
+            segment--;
+            cout << (segment*2) <<'\n';
+        }
+        else if (segment>1 && s[n-1]=='0')
+        {
+            cout << (segment*2)-1 <<'\n';
+        }
+        
+
+    }
+    
 
 
     return 0;

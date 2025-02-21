6baadb6f6d656df690a2de323cd99f938a2294f0
Shuchi28 <seven77bd@gmail.com>
2025-02-21 12:28:08 +0600

    Update B_Square.cpp

--- week-17/day-5/B_Square.cpp
+++ week-17/day-5/B_Square.cpp
@@ -3,7 +3,37 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
 
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        int mx1 = max(a, b);
+        int mx2 = max(c, d);
+        int mn1 = min(a, b);
+        int mn2 = min(c, d);
+
+        if (mx1==mx2)
+        {
+            if ((mn1+mn2)==mx1)
+            {
+                cout << "Yes\n";
+            }
+            else{
+                cout << "No\n";
+            }
+            
+        }
+        else{
+            cout << "No\n";
+        }
+        
+        
+    }
+    
 
     return 0;
 }

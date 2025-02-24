1c8de66ea8d281042f0159f63d60d231ab61f188
Shuchi28 <seven77bd@gmail.com>
2025-02-24 23:28:10 +0600

    Update B_Luntik_and_Subsequences.cpp

--- week-18(Level-2)/day-2/B_Luntik_and_Subsequences.cpp
+++ week-18(Level-2)/day-2/B_Luntik_and_Subsequences.cpp
@@ -6,7 +6,34 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+
+        for (int i=0; i<n; i++)
+        {
+            cin >> a[i];
+        }
+        
+        long long one = count(a.begin(), a.end(), 1);
+        long long zero = count(a.begin(), a.end(), 0);
+
+        if (one == 0)
+        {
+            cout << "0\n";
+        }
+        else{
+            cout << one*(1LL<<zero) <<'\n';
+        }
+
+        
+    }
+
 
     return 0;
 }

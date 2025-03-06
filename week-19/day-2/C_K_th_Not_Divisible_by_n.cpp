28a015d9e41fe60d23e4f7218e3fe2ede0300371
Shuchi28 <seven77bd@gmail.com>
2025-03-07 00:08:27 +0600

    Update C_K_th_Not_Divisible_by_n.cpp

--- week-19/day-2/C_K_th_Not_Divisible_by_n.cpp
+++ week-19/day-2/C_K_th_Not_Divisible_by_n.cpp
@@ -6,7 +6,36 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        auto ok = [&](int mid)
+        {
+            return (mid-(mid/n)) >= k;
+        };
+
+        int l=1, r=2e9, mid, ans;
+        while (l<=r)
+        {
+            mid = l + (r-l)/2;
+            if(ok(mid))
+            {
+                ans = mid;
+                r = mid -1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+
+        cout << ans <<'\n';
+        
+    }
 
     return 0;
 }

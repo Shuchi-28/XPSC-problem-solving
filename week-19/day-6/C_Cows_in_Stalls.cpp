75d16aa01213671a676cec9905bac3f2a46fca36
Shuchi28 <seven77bd@gmail.com>
2025-03-09 17:52:15 +0600

    Update C_Cows_in_Stalls.cpp

--- week-19/day-6/C_Cows_in_Stalls.cpp
+++ week-19/day-6/C_Cows_in_Stalls.cpp
@@ -6,6 +6,50 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int n, k;
+    cin >> n >> k;
+
+    vector<long long> v(n);
+
+    for (int i=0; i<n; i++)
+    {
+        cin >> v[i];
+    }
+    
+    auto ok = [&](int dis)
+    {
+        int cnt = 1;
+        for (int i=0; i<n; i++)
+        {
+            int j=i+1;
+            while (j<n && v[i]+dis>v[j])
+                j++;
+            if(j==n) 
+                break;
+            cnt++;
+            i = j-1;
+        }
+        
+        return cnt >= k;
+    };
+
+    int l=0, r = v[n-1]-v[0] + 10, mid, ans;
+
+    while (l<=r)
+    {
+        mid = l + (r-l)/2;
+        if(ok(mid))
+        {
+            ans = mid;
+            l = mid + 1;
+        }
+        else{
+            r = mid - 1;
+        }
+        
+    }
+    
+    cout << ans;
 
     return 0;
 }

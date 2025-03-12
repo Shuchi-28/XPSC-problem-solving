1520db32eb89bab1072363f1a958c37c904de032
Shuchi28 <seven77bd@gmail.com>
2025-03-12 23:23:07 +0600

    Update E_Building_an_Aquarium.cpp

--- week-20/day-1/E_Building_an_Aquarium.cpp
+++ week-20/day-1/E_Building_an_Aquarium.cpp
@@ -11,7 +11,42 @@ int main()
 
     while (t--)
     {
+        int n, x;
+        cin >> n >> x;
+
+        vector<long long> v(n);
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
+
+        auto ok = [&](long long h)
+        {
+            long long cnt = 0;
+            for (int i=0; i<n; i++)
+            {
+                cnt += h - min(h, v[i]);
+            }
+            return cnt<= x;
+
+        };
+
+        long long l=1, r=1e11, mid, ans;
+        while (l<=r)
+        {
+            mid = l + (r-l)/2;
+            if(ok(mid))
+            {
+                l = mid + 1;
+                ans = mid;
+            }
+            else{
+                r = mid - 1;
+            }
+            
+        }
         
+        cout << ans <<'\n';
 
     }
     

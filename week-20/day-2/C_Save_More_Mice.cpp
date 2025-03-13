ede14ccf7d66ff3f1dd5316df20196b8065f0dbe
Shuchi28 <seven77bd@gmail.com>
2025-03-14 01:13:20 +0600

    Update C_Save_More_Mice.cpp

--- week-20/day-2/C_Save_More_Mice.cpp
+++ week-20/day-2/C_Save_More_Mice.cpp
@@ -11,7 +11,29 @@ int main()
 
     while (t--)
     {
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> v(k);
+        for (int i=0; i<k; i++)
+        {
+            cin >> v[i];
+        }
+
+        sort(v.begin(), v.end());
+
+        int ans = 0, sum = 0;
+
+        for (int i=k-1; i>=0; i--)
+        {
+            sum += n - v[i];
+            if(sum<n)
+                ans++;
+            else
+                break;
+        }
         
+        cout << ans <<'\n';
 
     }
     

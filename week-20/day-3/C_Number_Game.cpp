444d69efeef760dcd8da3c1afa965fdded42d7ea
Shuchi28 <seven77bd@gmail.com>
2025-03-14 13:04:15 +0600

    Update C_Number_Game.cpp

--- week-20/day-3/C_Number_Game.cpp
+++ week-20/day-3/C_Number_Game.cpp
@@ -11,7 +11,66 @@ int main()
 
     while (t--)
     {
+        int n;
+        cin >> n;
+
+        vector<int> v(n);
+
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
+
+        auto ok = [&](int k)
+        {
+            multiset<int> s;
+            for(auto x: v)
+            {
+                s.insert(x);
+            }
+
+            for (int i=1; i<=k; i++)
+            {
+                if(s.empty())
+                    return false;
+                int req = k-i+1;
+
+                auto ii = s.upper_bound(req);
+                if(ii==s.begin())
+                    return false;
+                ii--;
+
+                s.erase(ii);
+                if(!s.empty())
+                {
+                    ii = s.begin();
+                    int a = (*ii);
+                    a += req;
+                    s.erase(ii);
+                    s.insert(a);
+                }
+            }
+
+            return true;
+            
+        };
+
+        int l=0, r=n, mid, ans;
+        while (l<=r)
+        {
+            mid = l + (r-l)/2;
+            if(ok(mid))
+            {
+                l = mid + 1;
+                ans = mid;
+            }
+            else{
+                r = mid - 1;
+            }
+            
+        }
         
+        cout << ans <<'\n';
 
     }
     

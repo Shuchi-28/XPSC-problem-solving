6a94e7a130c9cd79c69c5b13214cbc699bad5918
Shuchi28 <seven77bd@gmail.com>
2025-03-22 03:54:28 +0600

    Update Counting_Divisors.cpp

--- week-20/day-7/Counting_Divisors.cpp
+++ week-20/day-7/Counting_Divisors.cpp
@@ -1,9 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int maxN = 1e6+9;
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    vector<int> divisor(maxN);
+    for (int i=1; i<=maxN; i++)
+    {
+        for (int j= i; j<maxN; j+=i)
+        {
+            divisor[j]++;
+        }
+        
+    }
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        cout << divisor[n] <<'\n';
+    }
+    
 
     return 0;
 }

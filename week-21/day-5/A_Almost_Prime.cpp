da3a515a272048ccf776ea888da9b31c1bcd4623
Shuchi28 <seven77bd@gmail.com>
2025-03-26 13:34:48 +0600

    Update A_Almost_Prime.cpp

--- week-21/day-5/A_Almost_Prime.cpp
+++ week-21/day-5/A_Almost_Prime.cpp
@@ -3,7 +3,31 @@ using namespace std;
 
 int main()
 {
+    int n ,ans =0;
+    cin >> n;
 
+    map<int, int> mp;
+
+    for (int i=1; i<=n; i++)
+    {
+        int num = i;
+        set<int> st;
+        for (int j=2; j*j<=i; j++)
+        {
+            while (num%j == 0)
+            {
+                st.insert(j);
+                num /= j;
+            }
+        }
+        if(num>1)
+            st.insert(num);
+        if(st.size()==2)
+            ans++;
+    }
+
+    cout << ans;
+    
 
     return 0;
 }

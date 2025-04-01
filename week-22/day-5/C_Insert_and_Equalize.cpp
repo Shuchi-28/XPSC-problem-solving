10147684e4276dbeb46a02c1974c540cc2bd1fa6
Shuchi28 <seven77bd@gmail.com>
2025-04-01 13:09:11 +0600

    Update C_Insert_and_Equalize.cpp

--- week-22/day-5/C_Insert_and_Equalize.cpp
+++ week-22/day-5/C_Insert_and_Equalize.cpp
@@ -11,6 +11,60 @@ int main()
 
     while (t--)
     {
+        int n;
+        cin >> n;
+
+        vector<long long> a(n);
+        for (int i=0; i<n; i++)
+        {
+            cin >> a[i];
+        }
+
+        sort(a.begin(), a.end());
+
+        if(n==1)
+        {
+            cout << 1 <<'\n';
+            continue;
+        }
+        vector<long long> diff(n);
+        for (int i=0; i<n; i++)
+        {
+            diff[i] = a[n-1]-a[i];
+        }
+        
+        long long x = 0;
+
+        for (int i=0; i<n; i++)
+        {
+            x = __gcd(x, diff[i]);
+        }
+        
+        long long ans = 0;
+
+        for (int i=0; i<n; i++)
+        {
+            ans += diff[i]/x;
+        }
+        
+        set<long long> st;
+        for (int i=0; i<n; i++)
+        {
+            st.insert(a[i]);
+        }
+        
+        int step = 1;
+        while (true)
+        {
+            int p = a[n-1] - (step*x);
+
+            if(st.find(p)==st.end())
+                break;
+            step++;
+        }
+        
+        cout << ans+min(n, step) <<'\n';
+        
         
     }
     

a25f714c985f93a3b79980c2364ba857ae4351f1
Shuchi28 <seven77bd@gmail.com>
2025-04-05 13:11:46 +0600

    Update A_Mainak_and_Array.cpp

--- week-23/day-3/A_Mainak_and_Array.cpp
+++ week-23/day-3/A_Mainak_and_Array.cpp
@@ -5,16 +5,38 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
     int t;
     cin >> t;
 
-    while(t--)
+    while (t--)
     {
-        
-        
-    }
+        int n;
+        cin >> n;
 
+        vector<int> v(n);
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
+        int mx = v[n-1]-v[0];
+        
+        for (int i=1; i<n; i++)
+        {
+            mx = max(mx, v[i] - v[0]);
+        }
+        for (int i=0; i<n-1; i++)
+        {
+            mx = max(mx, v[n-1] - v[i]);
+        }
+        for (int i=0; i<n-1; i++)
+        {
+            mx = max(mx, v[i] - v[i+1]);
+        }
+        
+        cout << mx <<endl;
+    }
+    
 
     return 0;
 }

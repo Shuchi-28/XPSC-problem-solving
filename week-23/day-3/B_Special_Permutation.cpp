1a2c46fdce0331fffd7961bdb9bc3a8317e2cffc
Shuchi28 <seven77bd@gmail.com>
2025-04-05 18:15:39 +0600

    Update B_Special_Permutation.cpp

--- week-23/day-3/B_Special_Permutation.cpp
+++ week-23/day-3/B_Special_Permutation.cpp
@@ -11,7 +11,54 @@ int main()
 
     while(t--)
     {
+        int n, a, b;
+        cin >> n >> a >> b;
+
+        vector<int> arr;
+        arr.push_back(a);
+
+        int x = n;
+        for (int i=2; i<=n/2; i++)
+        {
+            while(x==a || x==b)
+                x--;
+            arr.push_back(x);
+            x--;
+        }
         
+        x = 1;
+        for (int i=n/2+1; i<=n; i++)
+        {
+            while(x==a || x==b)
+                x++;
+            arr.push_back(x);
+            x++;
+        }
+        arr[n-1] = b;
+        
+        int f = 0;
+        for(int i=0; i<n/2; i++)
+        {
+            if(arr[i]<a)
+                f = 1;
+        }
+        for(int i=n/2; i<n; i++)
+        {
+            if(arr[i]>b)
+                f = 1;
+        }
+            
+        if(f==1)
+        {
+            cout << "-1\n";
+        }
+        else{
+            for(int x: arr)
+            {
+                cout << x <<" ";
+            }
+            cout << '\n';
+        }
         
     }
 

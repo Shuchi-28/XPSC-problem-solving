aaa8543e5f021453bd06e9f5d16344dea5de70de
Shuchi28 <seven77bd@gmail.com>
2025-04-06 00:05:54 +0600

    Update B_Prefiquence.cpp

--- week-23/day-4/B_Prefiquence.cpp
+++ week-23/day-4/B_Prefiquence.cpp
@@ -8,11 +8,26 @@ int main()
 
     int t;
     cin >> t;
-
     while (t--)
     {
-        
+        int n, m;
+        cin >> n >> m;
 
+        string a, b;
+        cin >> a >> b;
+
+        int cnt = 0, j = 0;
+        for (int i=0; i<m; i++)
+        {
+            if (a[j]==b[i])
+            {
+                cnt++;
+                j++;
+            }
+            
+        }
+
+        cout << cnt <<'\n';
     }
     
 

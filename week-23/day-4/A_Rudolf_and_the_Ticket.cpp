36c48ae8441d590a16c7c1e7ddcc7ae25e6b5282
Shuchi28 <seven77bd@gmail.com>
2025-04-07 11:08:32 +0600

    Update A_Rudolf_and_the_Ticket.cpp

--- week-23/day-4/A_Rudolf_and_the_Ticket.cpp
+++ week-23/day-4/A_Rudolf_and_the_Ticket.cpp
@@ -11,8 +11,34 @@ int main()
 
     while (t--)
     {
-        
+        int n, m, k;
+        cin >> n >> m >> k;
 
+        vector<int> a(n), b(m);
+
+        for (int i=0; i<n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int i=0; i<m; i++)
+        {
+            cin >> b[i];
+        }
+        
+        int cnt=0;
+         
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<m; j++)
+            {
+                if(a[i]+b[j]<=k)
+                    cnt++;
+            }
+            
+        }
+        
+        cout << cnt << '\n';
+        
     }
     
 

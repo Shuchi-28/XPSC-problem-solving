976195980e54806c748c4954b654aab270f8d6f6
Shuchi28 <seven77bd@gmail.com>
2025-04-07 12:54:52 +0600

    Update B_Kana_and_Dragon_Quest_game.cpp

--- week-23/day-5/B_Kana_and_Dragon_Quest_game.cpp
+++ week-23/day-5/B_Kana_and_Dragon_Quest_game.cpp
@@ -6,7 +6,31 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
+    int t;
+    cin >> t;
 
+    while (t--)
+    {
+        int x, n, m;
+        cin >> x >> n >> m;
+
+        int h = x;
+        for (int i=0; i<n; i++)
+        {
+            x = (x/2)+10;
+        }
+        
+        x -= 10*m;
+        h -= 10*m;
+
+        if(x<=0 || h<=0)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+
+
+    }
+    
 
     return 0;
 }

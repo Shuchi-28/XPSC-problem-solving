cee28216a08ab921d57361bd33f9659fb058bc54
Shuchi28 <seven77bd@gmail.com>
2025-04-10 22:11:48 +0600

    Update B_Snow_Walking_Robot.cpp

--- week-23/day-6/B_Snow_Walking_Robot.cpp
+++ week-23/day-6/B_Snow_Walking_Robot.cpp
@@ -11,8 +11,54 @@ int main()
 
     while (t--)
     {
-        
+        string s, ans="";
+        cin >> s;
 
+        int L = count(s.begin(), s.end(), 'L');
+        int R = count(s.begin(), s.end(), 'R');
+        int U = count(s.begin(), s.end(), 'U');
+        int D = count(s.begin(), s.end(), 'D');
+
+        int minLR = min(L, R);
+        int minUD = min(U, D);
+
+        if(minLR==0 && minUD==0)
+        {
+            cout << 0 <<'\n';
+            cout << " ";
+        }
+        else if(minLR==0)
+        {
+            cout << 2 <<'\n';
+            cout << "UD\n";
+        }
+        else if (minUD==0)
+        {
+            cout << 2 <<'\n';
+            cout << "LR\n";
+        }
+        else{
+            for (int i=0; i<minUD; i++)
+            {
+                ans += "U";
+            }
+            for (int i=0; i<minLR; i++)
+            {
+                ans += "R";
+            }
+            for (int i=0; i<minUD; i++)
+            {
+                ans += "D";
+            }
+            for (int i=0; i<minLR; i++)
+            {
+                ans += "L";
+            }
+            
+            cout << ans.size() <<'\n';
+            cout << ans <<'\n';
+        }
+        
     }
     
 

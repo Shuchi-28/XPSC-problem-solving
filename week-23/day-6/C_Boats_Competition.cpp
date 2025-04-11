d5118b48bbb7537fa06f933949474cd3def98e73
Shuchi28 <seven77bd@gmail.com>
2025-04-11 12:18:20 +0600

    Update C_Boats_Competition.cpp

--- week-23/day-6/C_Boats_Competition.cpp
+++ week-23/day-6/C_Boats_Competition.cpp
@@ -11,8 +11,34 @@ int main()
 
     while (t--)
     {
+        int n, ans= 0;
+        cin >> n;
+
+        vector<int> v(n);
+        map<int, int> mp;
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+            mp[v[i]]++;
+        }
         
 
+        for (int i=2; i<=(n*2); i++)
+        {
+            int cnt = 0;
+            for (auto j: mp)
+            {
+                int other = i - j.first;
+                if(other>=1 && mp.count(other))
+                    cnt += min(j.second, mp[other]);
+            }
+            cnt /= 2;
+
+            ans = max(ans, cnt);
+        }
+       
+
+        cout << ans <<'\n';
     }
     
 

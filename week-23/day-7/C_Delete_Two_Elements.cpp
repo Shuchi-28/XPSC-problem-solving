96a431bfc57712338ce0a6f29c5f1fe3ac7047f8
Shuchi28 <seven77bd@gmail.com>
2025-04-12 01:29:13 +0600

    Update C_Delete_Two_Elements.cpp

--- week-23/day-7/C_Delete_Two_Elements.cpp
+++ week-23/day-7/C_Delete_Two_Elements.cpp
@@ -11,7 +11,34 @@ int main()
 
     while (t--)
     {
+        int n;
+        cin >> n;
+
+        vector<long long> v(n);
+        long long sum = 0, ans = 0;
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+            sum += v[i];
+        }
         
+        map<int, int> mp;
+        long long req = 2*sum/n;
+
+        if((sum-req)*n != sum*(n-2))
+        {
+            cout << 0 <<'\n';
+            continue;
+        }
+        long long x = 2*sum/n;
+
+        for (int i=0; i<n; i++)
+        {
+            ans += mp[req - v[i]];
+            mp[v[i]]++;
+        }
+        
+        cout << ans <<'\n';
 
     }
     

9c16753896d6074ba4e51c0de9ba0e8f74af851c
Shuchi28 <seven77bd@gmail.com>
2025-04-15 01:40:50 +0600

    Update E_Klever_Permutation.cpp

--- week-24/day-1/E_Klever_Permutation.cpp
+++ week-24/day-1/E_Klever_Permutation.cpp
@@ -11,8 +11,43 @@ int main()
 
     while (t--)
     {
+        int n, k;
+        cin >> n >> k;
         
+        vector<int> v(n);
+        int x = 1;
+        for (int i=0; i<k; i++)
+        {
+            if(i%2==0)
+            {
+                for (int j=i; j<n; j+=k)
+                {
+                    v[j] = x;
+                    x++;
+                }
+            }
+            else{
+                int l = i;
+                while (l+k<n)
+                {
+                    l += k;
+                }
+                for (int j=l; j>=0; j-=k)
+                {
+                    v[j] = x;
+                    x++;
+                }
+                
+            }
+        }
 
+        for (int i=0; i<n; i++)
+        {
+            cout << v[i] <<" ";
+        }
+            
+        cout <<'\n';
+        
     }
     
 

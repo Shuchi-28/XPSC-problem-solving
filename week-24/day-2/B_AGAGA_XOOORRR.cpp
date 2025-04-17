95eea19369d8c721ba40b6d65a1fe3c804ce24c3
Shuchi28 <seven77bd@gmail.com>
2025-04-17 23:01:58 +0600

    Update B_AGAGA_XOOORRR.cpp

--- week-24/day-2/B_AGAGA_XOOORRR.cpp
+++ week-24/day-2/B_AGAGA_XOOORRR.cpp
@@ -11,7 +11,48 @@ int main()
 
     while (t--)
     {
+        int n;
+        cin >> n;
+
+        vector<int> v(n+1), pre(n+1);
+        for (int i=1; i<=n; i++)
+        {
+            cin >> v[i];
+        }
+
+        pre[0] = 0;
+        for (int i=1; i<=n; i++)
+        {
+            pre[i] = pre[i-1] ^ v[i];
+        }
+
+        int flag = 0;
+
+        for (int i=1; i<n; i++)
+        {
+            int a = pre[i];
+            int b = pre[n] ^ pre[i];
+            if(a==b)
+                flag = 1;
+        }
+
+        for (int i=1; i<n-1; i++)
+        {
+            int a = pre[i];
+            for (int j=i+1; j<n; j++)
+            {
+                int b = pre[j] ^ pre[i];
+                int c = pre[n] ^ pre[j];
+                if(a==b && b==c)
+                    flag = 1;
+            }
+            
+        }
         
+        if(flag)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
         
     }
     

ca5fca840921c65d67b160c97d63e150ed9115f7
Shuchi28 <seven77bd@gmail.com>
2025-04-19 21:03:51 +0600

    Update F_Quests.cpp

--- week-24/day-2/F_Quests.cpp
+++ week-24/day-2/F_Quests.cpp
@@ -11,8 +11,95 @@ int main()
 
     while (t--)
     {
+        long long n, c, d;
+        cin >> n >> c >> d;
+
+        vector<long long> v(n);
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
         
+        sort(v.begin(), v.end(), greater<long long>());
+
+        auto ok = [&](long long k)
+        {
+            long long sum = 0, gain = 0;
+            if(k==0)
+                return(v[0]*d>=c);
+
+            if(k>=n)
+            {
+                for (int i=0; i<n; i++)
+                {
+                    sum += v[i];
+                }
+            }
+            else{
+                for (int i=0; i<k; i++)
+                {
+                    sum += v[i];
+                }
+            }
+            
+            long long times = d/k;
+            gain = times * sum;
+
+            long long rem = d%k;
+            if(rem>=n)
+            {
+                for (int i=0; i<n; i++)
+                {
+                    gain += v[i];
+                }
+            }
+            else {
+                for (int i=0; i<rem; i++)
+                {
+                    gain += v[i];
+                }
+            }
+
+            return (gain>=c);
+            
+        };
+
+        long long sum = 0;
+        for (int i=0; i<min(d,n); i++)
+        {
+            sum += v[i];
+        }
+
+        if(sum>=c)
+        {
+            cout << "Infinity\n";
+            continue;
+        }
+
+        long long l = 0, r= 1e12, mid, ans = -1;
+
+        while (l<=r)
+        {
+            mid = l + (r-l)/2;
+            if(ok(mid+1))
+            {
+                ans = mid;
+                l = mid + 1;
+            }
+            else{
+                r = mid - 1;
+            }
+        }
         
+        if(ans == -1)
+        {
+            cout << "Impossible\n";
+        }
+        else{
+            cout << ans <<'\n';
+        }
+        
+
     }
     
 

285f08b8feec0848dfe75c0632bf34801263877d
Shuchi28 <seven77bd@gmail.com>
2025-04-28 21:09:21 +0600

    Update C_Little_Girl_and_Maximum_Sum.cpp

--- week-24/day-3/C_Little_Girl_and_Maximum_Sum.cpp
+++ week-24/day-3/C_Little_Girl_and_Maximum_Sum.cpp
@@ -6,15 +6,42 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t;
-    cin >> t;
+    int n, q;
+    cin >> n >> q;
 
-    while (t--)
+    vector<long long> arr(n);
+    for (int i=0; i<n; i++)
     {
-        
-        
+        cin >> arr[i];
     }
-    
+        
+    vector<long long> dif(n+1);
+    for (int i=0; i<q; i++)
+    {
+        int l, r;
+        cin >> l >> r;
+
+        l--;
+        r--;
+        dif[l]++;
+        dif[r+1]--;
+    }
+        
+    for (int i=1; i<=n; i++)
+    {
+        dif[i] = dif[i-1]+dif[i];
+    }
+    sort(arr.rbegin(), arr.rend());
+    sort(dif.rbegin(), dif.rend());
+
+    int long long ans = 0;
+    for (int i=0; i<n; i++)
+    {
+       ans += (1LL*arr[i]*dif[i]);
+    }
+        
+    cout << ans;
+        
     
     return 0;
 }

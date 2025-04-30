34f4f4f7b0c18df6c3c5d7ae52e57f3b3e4fbee3
Shuchi28 <seven77bd@gmail.com>
2025-05-01 00:01:04 +0600

    Update C_Two_TVs.cpp

--- week-24/day-4/C_Two_TVs.cpp
+++ week-24/day-4/C_Two_TVs.cpp
@@ -6,15 +6,32 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t;
-    cin >> t;
+    int n;
+    cin >> n;
 
-    while (t--)
+    map<int, int> dif;
+    for(int i=0; i<n; i++)
     {
-        
-        
-    }
-    
+        int l, r;
+        cin >> l >> r;
 
+        dif[l]++;
+        dif[r+1]--;
+    }
+
+    long long flag=0, sum=0;
+    for(auto x: dif)
+    {
+        sum += x.second;
+        if(sum>2)
+            flag = 1;
+    }
+
+    if(flag)
+        cout << "NO\n";
+    else
+        cout << "YES\n";
+
+        
     return 0;
 }

4f0a1e9b7c497ce41dcf2e14fcf6e64d479bfe6c
Shuchi28 <seven77bd@gmail.com>
2025-05-02 00:53:04 +0600

    Update Inversion_Count.cpp

--- week-25/day-2/Inversion_Count.cpp
+++ week-25/day-2/Inversion_Count.cpp
@@ -1,20 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
 
-int main()
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int main() 
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     int t;
     cin >> t;
 
-    while (t--)
+    while(t--)
     {
-        
-        
-    }
-    
+        pbds<int> p;
+        int n, cnt=0;
+        cin >> n;
 
-    return 0;
+        vector<int> v(n);
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
+
+        for (int i=n-1; i>=0; i--)
+        {
+            cnt += p.order_of_key(v[i]);
+            p.insert(v[i]);
+        }
+        
+        cout << cnt <<'\n';
+    }
+
+
+
+   return 0;
 }

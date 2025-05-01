d2c08fb6f46565cab5381d460377a44842574f15
Shuchi28 <seven77bd@gmail.com>
2025-05-01 22:58:54 +0600

    Update Sliding_Window_Median.cpp

--- week-25/day-3/Sliding_Window_Median.cpp
+++ week-25/day-3/Sliding_Window_Median.cpp
@@ -1,20 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
 using namespace std;
 
-int main()
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main() 
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t;
-    cin >> t;
+    int n, k;
+    cin >> n >> k;
 
-    while (t--)
+    vector<int> v(n);
+
+    for (int i=0; i<n; i++)
     {
-        
-
+        cin >> v[i];
     }
     
+    int l=0, r=0;
+    pbds<pair<int,int>> a;
 
-    return 0;
+    while (r<n)
+    {
+        a.insert({v[r], r});
+        if(r-l+1 == k)
+        {
+            int pos = k/2;
+            if(k%2==0)
+                pos--;
+            cout << a.find_by_order(pos)->first <<" ";
+            a.erase({v[l], l});
+            l++;
+        }
+        r++;
+    }
+    
+     
+
+   return 0;
 }

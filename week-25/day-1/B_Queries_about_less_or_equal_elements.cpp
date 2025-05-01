a46cde563525be41937c40f61ec4fdb29a41db2b
Shuchi28 <seven77bd@gmail.com>
2025-05-01 12:54:55 +0600

    Update B_Queries_about_less_or_equal_elements.cpp

--- week-25/day-1/B_Queries_about_less_or_equal_elements.cpp
+++ week-25/day-1/B_Queries_about_less_or_equal_elements.cpp
@@ -1,20 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
 using namespace std;
 
-int main()
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main() 
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int t;
-    cin >> t;
-
-    while (t--)
+    int n, m;
+    cin >> n >> m;
+    pbds<int> a;
+    for (int i=0; i<n; i++)
     {
-        
-        
+        int x;
+        cin >> x;
+        a.insert(x);
     }
-    
 
-    return 0;
+    for (int i=0; i<m; i++)
+    {
+        int x;
+        cin >> x;
+        cout << a.order_of_key(x+1) << " ";
+    }
+     
+
+
+   return 0;
 }

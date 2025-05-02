a88641e0a9ab41ec9e81706a2247546543fc791e
Shuchi28 <seven77bd@gmail.com>
2025-05-02 19:59:51 +0600

    Update Restaurant_Customers.cpp

--- week-24/day-5/Restaurant_Customers.cpp
+++ week-24/day-5/Restaurant_Customers.cpp
@@ -6,14 +6,26 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t;
-    cin >> t;
+    int n;
+    cin >> n;
 
-    while (t--)
+    map<long, long> mp;
+    for (int i=0; i<n; i++)
     {
-        
-        
+        long long l, r;
+        cin >> l >> r;
+        mp[l]++;
+        mp[r+1]--;
     }
+    
+    int ans=INT_MIN, sum = 0;
+    for(auto x: mp)
+    {
+        sum += x.second;
+        ans = max(ans, sum);
+    }
+
+    cout << ans;
     
 
     return 0;

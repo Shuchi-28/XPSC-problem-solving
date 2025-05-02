8675fe87d570dd9f205aeefdd4355e9fbba2f1ff
Shuchi28 <seven77bd@gmail.com>
2025-05-02 18:23:59 +0600

    Update E_String_Reversal.cpp

--- week-25/day-4/E_String_Reversal.cpp
+++ week-25/day-4/E_String_Reversal.cpp
@@ -1,21 +1,52 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
 using namespace std;
 
-int main()
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int main() 
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int t;
-    cin >> t;
+    int n;
+    cin >> n;
+    
+    string s, r;
+    cin >> s;
+    r = s;
+    reverse(r.begin(), r.end());
 
-    while (t--)
+    map<char, vector<int>> posr, poss;
+    for (int i=0; i<n; i++)
     {
-        
-        
+        poss[s[i]].push_back(i);
+        posr[r[i]].push_back(i);
     }
     
+    vector<int> permutation(n);
+    for (char ch='a'; ch<='z'; ch++)
+    {
+        for (int i=0; i<poss[ch].size(); i++)
+        {
+            permutation[posr[ch][i]] = poss[ch][i];
+        }
+    }
+    
+    pbds<int> p;
+    long long ans = 0;
+
+    for (int i=n-1; i>=0; i--)
+    {
+        ans += p.order_of_key(permutation[i]);
+        p.insert(permutation[i]);
+    }
+    
+    cout << ans <<'\n';
 
 
-    return 0;
+   return 0;
 }

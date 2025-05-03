38d4a8c6d02cc78063b0298ff1f4a77f1e396100
Shuchi28 <seven77bd@gmail.com>
2025-05-03 13:00:44 +0600

    Update B_Karen_and_Coffee.cpp

--- week-24/day-6/B_Karen_and_Coffee.cpp
+++ week-24/day-6/B_Karen_and_Coffee.cpp
@@ -6,15 +6,40 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t;
-    cin >> t;
+    int n, k, q;
+    cin >> n >> k >> q;
 
-    while (t--)
+    vector<int> v(2e5+10, 0), good(2e5+10, 0);
+    for (int i=0; i<n; i++)
     {
-        
-
+        int l, r;
+        cin >> l >> r;
+        v[l]++;
+        v[r+1]--;
     }
-    
+        
+    for (int i=1; i<2e5+10; i++)
+    {
+        v[i]= v[i]+v[i-1];
+    }
+    for (int i=0; i<2e5+10; i++)
+    {
+        if(v[i]>=k)
+            good[i] = 1;
+    }
+    for (int i=1; i<2e5+10; i++)
+    {
+        good[i]= good[i]+good[i-1];
+    }
 
+    for (int i=0; i<q; i++)
+    {
+        int l, r, sum=0;
+        cin >> l >> r;
+        
+        cout << good[r] - good[l-1] << '\n';
+    }
+        
+    
     return 0;
 }

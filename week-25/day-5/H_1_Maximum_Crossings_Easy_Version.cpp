d20b95303d73da82cb3d43c53c74e578b6a7840f
Shuchi28 <seven77bd@gmail.com>
2025-05-08 19:14:52 +0600

    Update H_1_Maximum_Crossings_Easy_Version.cpp

--- week-25/day-5/H_1_Maximum_Crossings_Easy_Version.cpp
+++ week-25/day-5/H_1_Maximum_Crossings_Easy_Version.cpp
@@ -1,7 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
 using namespace std;
 
-int main()
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int main() 
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -11,11 +17,28 @@ int main()
 
     while (t--)
     {
-        
-        
+        int n;
+        cin >> n;
+
+        vector<int> v(n);
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
+
+        pbds<int> p;
+        int ans = 0;
+        for (int i=n-1; i>=0; i--)
+        {
+            ans += p.order_of_key(v[i]+1);
+            p.insert(v[i]);
+        }
+
+        cout << ans <<'\n';
+
     }
     
 
 
-    return 0;
+   return 0;
 }

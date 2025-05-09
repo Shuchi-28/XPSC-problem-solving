20b268bd7f78c484e8842e2392f52ab37dca1e52
Shuchi28 <seven77bd@gmail.com>
2025-05-09 21:34:05 +0600

    Update E_Enemy_is_weak.cpp

--- week-26/day-2/E_Enemy_is_weak.cpp
+++ week-26/day-2/E_Enemy_is_weak.cpp
@@ -1,15 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
 using namespace std;
 
-int main()
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename T> using gpbds = tree<T, null_type, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int main() 
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     int n;
     cin >> n;
 
-    
-        
-    return 0;
+    vector<long long> v(n);
+    pbds<long long> a;
+    gpbds<long long> b;
+    for (int i=0; i<n; i++)
+    {
+        cin >> v[i];
+        a.insert(v[i]);
+    }
+
+    long long x, y, ans=0;
+    for (int i=0; i<n; i++)
+    {
+        a.erase(v[i]);
+        y = a.order_of_key(v[i]);
+        x = b.order_of_key(v[i]);
+        b.insert(v[i]);
+        ans += x*y;
+    }
+
+    cout << ans;
+
+   return 0;
 }

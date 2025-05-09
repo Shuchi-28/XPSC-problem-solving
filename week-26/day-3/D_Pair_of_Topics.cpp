b4cc7f3b9b2ecc51f11e929182808286830c7117
Shuchi28 <seven77bd@gmail.com>
2025-05-10 00:10:50 +0600

    Update D_Pair_of_Topics.cpp

--- week-26/day-3/D_Pair_of_Topics.cpp
+++ week-26/day-3/D_Pair_of_Topics.cpp
@@ -1,15 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
 using namespace std;
 
-int main()
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int main() 
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     int n;
     cin >> n;
+    vector<long long> a(n), b(n), c(n);
 
+    for (int i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i=0; i<n; i++)
+    {
+        cin >> b[i];
+    }
+    for (int i=0; i<n; i++)
+    {
+        c[i] = b[i]-a[i];
+    }
 
-        
-    return 0;
+    pbds<long long> p;
+    for (int i=0; i<n; i++)
+    {
+        c[i] = b[i]-a[i];
+    }
+    
+    long long ans = 0;
+
+    for (int i=0; i<n; i++)
+    {
+        ans += p.order_of_key(-1*c[i]);
+        p.insert(c[i]);
+    }
+    
+    cout << ans;
+
+   return 0;
 }

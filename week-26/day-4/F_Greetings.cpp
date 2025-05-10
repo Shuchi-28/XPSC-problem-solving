b062a78e783961761933f54628b61719602273d5
Shuchi28 <seven77bd@gmail.com>
2025-05-11 02:24:25 +0600

    Update F_Greetings.cpp

--- week-26/day-4/F_Greetings.cpp
+++ week-26/day-4/F_Greetings.cpp
@@ -1,15 +1,50 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
 using namespace std;
 
-int main()
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+int main() 
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
-    int n;
-    cin >> n;
-
     
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<vector<int>> v(n, vector<int>(2));
+        for (int i=0; i<n; i++)
+        {
+            cin >> v[i][0] >> v[i][1];
+        }
+
+        sort(v.begin(), v.end());
+
+        pbds<int> p;
+        for (int i=0; i<n; i++)
+        {
+            p.insert(v[i][1]);
+        }
         
-    return 0;
+        long long ans = 0;
+        for (int i=0; i<n; i++)
+        {
+            ans += p.order_of_key(v[i][1]);
+            p.erase(v[i][1]);
+        }
+
+        cout << ans <<'\n';
+        
+    }
+     
+
+   return 0;
 }

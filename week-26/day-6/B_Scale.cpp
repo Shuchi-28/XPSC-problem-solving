1de16e71121681b0b5d61045e632926446f674eb
Shuchi28 <seven77bd@gmail.com>
2025-05-11 21:10:32 +0600

    Update B_Scale.cpp

--- week-26/day-6/B_Scale.cpp
+++ week-26/day-6/B_Scale.cpp
@@ -11,6 +11,27 @@ int main()
 
     while (t--)
     {
+        int n, k;
+        cin >> n >> k;
+
+        char arr[n][n];
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<n; j++)
+            {
+                cin >> arr[i][j];
+            }
+            
+        }
+
+        for (int i=0; i<n; i+=k)
+        {
+            for (int j=0; j<n; j+=k)
+            {
+                cout << arr[i][j];
+            }
+            cout <<'\n';
+        }
         
         
     }

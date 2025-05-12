53c3d54de6c517a4a558fdc5c66892b343e22be2
Shuchi28 <seven77bd@gmail.com>
2025-05-12 22:36:37 +0600

    Update B_Mike_and_Children.cpp

--- week-27/day-2/B_Mike_and_Children.cpp
+++ week-27/day-2/B_Mike_and_Children.cpp
@@ -6,16 +6,34 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int t;
-    cin >> t;
+    int n;
+    cin >> n;
 
-    while (t--)
+    vector<int> v(n);
+    for (int i=0; i<n; i++)
     {
-        
-        
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+
+    map<long long, int> mp;
+    
+    for (int i=0; i<n; i++)
+    {
+        for (int j=i+1; j<n; j++)
+        {
+            mp[v[i]+v[j]]++;
+        }
     }
     
+    int ans=0;
+    for (auto x: mp)
+    {
+        ans = max(ans, x.second);
+    }
 
+    cout << ans;
+    
 
     return 0;
 }

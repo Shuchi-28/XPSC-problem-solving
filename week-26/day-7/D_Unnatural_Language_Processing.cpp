cfc9711be050c0363b7e1d5245cd2197216a9d96
Shuchi28 <seven77bd@gmail.com>
2025-05-12 20:59:23 +0600

    Update D_Unnatural_Language_Processing.cpp

--- week-26/day-7/D_Unnatural_Language_Processing.cpp
+++ week-26/day-7/D_Unnatural_Language_Processing.cpp
@@ -5,16 +5,41 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
     int t;
     cin >> t;
 
     while (t--)
     {
-        
-        
+        int n;
+        cin >> n;
+
+        string ss;
+        cin >> ss;
+
+        for (int i=0; i<n; i++)
+        {
+            if (ss[i]=='a'|| ss[i]=='e')
+            {
+                if ((ss[i+3]=='a'||ss[i+3]=='e') && i+3<n)
+                {
+                    cout << ss[i] << ss[i+1] << ".";
+                    i++;
+                }
+                else{
+                    cout << ss[i];
+                    if(i!=n-1 && (i+2) != n) 
+                        cout << ".";
+                }
+            }
+            else{
+                cout << ss[i];
+            }
+        }
+        cout <<'\n';
+
     }
-    
+
 
 
     return 0;

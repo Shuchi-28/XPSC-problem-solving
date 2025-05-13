7abecd132fa6316866ccf7994560f24d60f7e680
Shuchi28 <seven77bd@gmail.com>
2025-05-13 22:10:40 +0600

    Update B_Game_with_string.cpp

--- week-27/day-3/B_Game_with_string.cpp
+++ week-27/day-3/B_Game_with_string.cpp
@@ -6,15 +6,27 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int t;
-    cin >> t;
+    string s;
+    cin >> s;
 
-    while (t--)
+    int cnt = 0;
+    stack<int> st;
+    
+    for (int i=0; i<s.size(); i++)
     {
-        
-        
+        if(!st.empty() && s[i]==st.top())
+        {
+            st.pop();
+            cnt++;
+        }
+        else
+            st.push(s[i]);
     }
     
+    if(cnt%2 == 0)
+        cout << "No\n";
+    else
+        cout << "Yes\n";
 
 
     return 0;

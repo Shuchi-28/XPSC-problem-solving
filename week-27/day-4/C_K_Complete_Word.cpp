2fd2b155b2e9be597f186f77dfc555a53d807252
Shuchi28 <seven77bd@gmail.com>
2025-05-16 12:09:35 +0600

    Update C_K_Complete_Word.cpp

--- week-27/day-4/C_K_Complete_Word.cpp
+++ week-27/day-4/C_K_Complete_Word.cpp
@@ -11,7 +11,47 @@ int main()
 
     while (t--)
     {
-        
+        int n, k;
+        cin >> n >> k;
+
+        string s;
+        cin >> s;
+
+        int ans = 0;
+        for (int i=0; i<k/2; i++)
+        {
+            vector<int> freq(26,0);
+            for (int j=0; j+k-1<n; j+=k)
+            {
+                int i1 = i+j;
+                int i2 = j+(k-i-1);
+
+                freq[s[i1]-'a']++;
+                freq[s[i2]-'a']++;
+            }
+
+            int req = 2*(n/k);
+            int mx = *max_element(freq.begin(), freq.end());
+
+            ans += req - mx;
+        }
+
+        if(k&1)
+        {
+            vector<int> freq(26,0);
+            for (int i=k/2; i<n; i+=k)
+            {
+                freq[s[i]-'a']++;
+
+            }
+
+            int req = (n/k);
+            int mx = *max_element(freq.begin(), freq.end());
+
+            ans += req - mx;
+        }
+
+        cout << ans <<'\n';
         
     }
     

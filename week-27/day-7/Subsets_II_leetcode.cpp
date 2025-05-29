73eefc231ec280235a08dfa0d05d2c6aaa26add3
Shuchi28 <seven77bd@gmail.com>
2025-05-30 00:00:42 +0600

    Update Subsets_II_leetcode.cpp

--- week-27/day-7/Subsets_II_leetcode.cpp
+++ week-27/day-7/Subsets_II_leetcode.cpp
@@ -1,1 +1,31 @@
 //https://leetcode.com/problems/subsets-ii/description/
+class Solution {
+public:
+    void f(int i, vector<int> &nums, vector<int> &a, vector<vector<int>> &ans, int n)
+    {
+        if(i==n)
+        {
+            ans.push_back(a);
+            return;
+        }
+
+        a.push_back(nums[i]);
+        f(i+1, nums, a, ans, n);
+        a.pop_back();
+
+        int indx = i+1;
+        while(indx<n && nums[i]==nums[indx])
+            indx++;
+        f(indx, nums, a, ans, n);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans; 
+        int n = nums.size();
+        vector<int> a;
+        
+        f(0, nums, a, ans, n);
+
+        return ans;
+    }
+};

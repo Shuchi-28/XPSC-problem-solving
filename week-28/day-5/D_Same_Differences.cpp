78ba480ed6ec36eafd9c28427ce5099baa4c1088
Shuchi28 <seven77bd@gmail.com>
2025-06-03 17:17:35 +0600

    Update D_Same_Differences.cpp

--- week-28/day-5/D_Same_Differences.cpp
+++ week-28/day-5/D_Same_Differences.cpp
@@ -11,8 +11,26 @@ int main()
 
     while (t--)
     {
+        int n;
+        cin >> n;
+
+        map<int, int> mp;
+        for (int i=0; i<n; i++)
+        {
+            int x;
+            cin >> x;
+            mp[x-i]++;
+        }
         
+        long long ans = 0;
+        for(auto a: mp)
+        {
+            long long cnt = a.second;
+            ans += (cnt*(cnt-1))/2;
+        }
         
+        cout << ans <<'\n';
+
     }
 
 
